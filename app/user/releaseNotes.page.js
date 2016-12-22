57e072452dcdc8121ed0d7c27fb2cc65b12f7a24
Tomas Hrstka <HrstkaTomas@seznam.cz>
2016-12-22 11:09:34 +0100

    Heroku webpack Environment variables fixed.

--- app/user/releaseNotes.page.js
+++ app/user/releaseNotes.page.js
@@ -24,7 +24,6 @@ export default class ReleaseNotesPage extends Component {
         {/*<button onClick={onClick}>Generate PDF</button>*/}
 
         {/* Release notes ======================================================*/}
-        auto
         <div>
           <RoundButton content={<span className="fa fa-chevron-left" aria-hidden="true"></span>} onClick={onBack}/>
         </div>
