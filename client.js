69c70134c48e5e2fe512d803309da3f63b3868e9
Ing. Tomas Hrstka <hrstkatomas@seznam.cz>
2017-01-12 14:50:12 +0100

    Herokut TCP testing / client.js changes

--- client.js
+++ client.js
@@ -1,6 +1,11 @@
 var net = require('net');
+var os = require('os')
+
+console.log(os.networkInterfaces());
+
 
 var client = new net.Socket();
+
 client.connect(5060, process.argv[2], function() { //'192.168.136.127'
 	console.log('Connected');
 	client.write('Hello, server! Love, Client.');
@@ -10,6 +15,16 @@ client.on('data', function(data) {
 	console.log('Received: ' + data);
 	// client.destroy(); // kill client after server's response
 });
+
+client.on('error', function(data) {
+	console.log('Error: ' + data);
+});
+
+client.on('timeout', function(data) {
+	console.log('timeout: ' + data);
+});
+
+
 
 client.on('close', function() {
 	console.log('Connection closed');
