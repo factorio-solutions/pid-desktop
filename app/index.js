dce9d1338c12388a0edb39adc539b204fe5e80d5
Ing. Tomas Hrstka <hrstkatomas@seznam.cz>
2017-06-16 13:54:16 +0200

    Api URL changed to Heroku testing application

--- app/index.js
+++ app/index.js
@@ -14,9 +14,9 @@ import './_styles/app.desktop.scss'
 export const store = configureStore()
 export const mobile = false  // when different actions are needed on mobile and desktop
 // export const entryPoint = (process.env.API_ENTRYPOINT || 'http://localhost:3000')+'/queries'
-export const entryPoint = ('https://park-it-direct.herokuapp.com')+'/queries'
+// export const entryPoint = ('https://park-it-direct.herokuapp.com')+'/queries'
 // export const entryPoint = 'https://park-it-direct.herokuapp.com/queries'
-// export const entryPoint = 'https://park-it-direct-alpha.herokuapp.com/queries'
+export const entryPoint = 'https://park-it-direct-alpha.herokuapp.com/queries'
 // export const entryPoint = 'http://localhost:3000/queries'
 const history = syncHistoryWithStore(hashHistory, store)
 
