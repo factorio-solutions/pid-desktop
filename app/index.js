82f68beb626c6f7f89346e4c73814a50a41b01a4
Ing. Tomas Hrstka <hrstkatomas@seznam.cz>
2017-06-16 14:59:13 +0200

    API URL changed for Testing repository

--- app/index.js
+++ app/index.js
@@ -15,8 +15,8 @@ export const store = configureStore()
 export const mobile = false  // when different actions are needed on mobile and desktop
 // export const entryPoint = (process.env.API_ENTRYPOINT || 'http://localhost:3000')+'/queries'
 // export const entryPoint = ('https://park-it-direct.herokuapp.com')+'/queries'
-export const entryPoint = 'https://park-it-direct.herokuapp.com/queries'
-// export const entryPoint = 'https://park-it-direct-alpha.herokuapp.com/queries'
+// export const entryPoint = 'https://park-it-direct.herokuapp.com/queries'
+export const entryPoint = 'https://park-it-direct-alpha.herokuapp.com/queries'
 // export const entryPoint = 'http://localhost:3000/queries'
 const history = syncHistoryWithStore(hashHistory, store)
 
