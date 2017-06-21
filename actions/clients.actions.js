b242429715995348a679e80131d7a7df1b6e75f4
Ing. Tomas Hrstka <hrstkatomas@seznam.cz>
2017-06-21 15:00:51 +0200

    Fix: Majitel garáže nevidí agreementy v jeho garáži clientů ve kterých není členem

--- actions/clients.actions.js
+++ actions/clients.actions.js
@@ -1,14 +1,21 @@
 import { request } from '../helpers/request'
 import _           from 'lodash'
 
-import { GET_CLIENTS } from '../queries/clients.queries'
+import { GET_CLIENTS, GARAGE_CONTRACTS } from '../queries/clients.queries'
 
-export const SET_CLIENTS = "SET_GARAGES"
+export const SET_CLIENTS          = 'SET_GARAGES'
+export const SET_GARAGE_CONTRACTS = 'SET_GARAGE_CONTRACTS'
 
 
 export function setClients (clients){
   return  { type: SET_CLIENTS
           , value: clients
+          }
+}
+
+export function setGarageContracts (value){
+  return  { type: SET_GARAGE_CONTRACTS
+          , value
           }
 }
 
@@ -23,12 +30,29 @@ export function initClients (){
                                 .map(function (client_users) {return client_users.client.id})
 
       uniqueClients.map((client) => {
-        managebleClientIds.includes(client.id) ? client.admin = true : client.admin = false
+        client.admin = managebleClientIds.includes(client.id)
+        client.userOfClient = true
         return client
       })
 
       dispatch( setClients( uniqueClients ) )
     }
     request(onSuccess, GET_CLIENTS)
+  }
+}
+
+export function initGarageContracts (){
+  return (dispatch, getState) => {
+    const onSuccess = (response) => {
+      const uniqueClients = response.data.garage.contracts.reduce((acc, contract) => {
+        if (!acc.hasOwnProperty(contract.client.id)) acc[contract.client.id] = {...contract.client, userOfClient: false, admin: false, contracts: [] }
+        acc[contract.client.id].contracts.push({id: contract.id, name: contract.name})
+        return acc
+      }, [])
+
+      dispatch(setGarageContracts(uniqueClients))
+    }
+
+    request(onSuccess, GARAGE_CONTRACTS, {id: getState().pageBase.garage})
   }
 }
--- reducers/clients.reducer.js
+++ reducers/clients.reducer.js
@@ -1,15 +1,22 @@
-import { SET_CLIENTS }  from '../actions/clients.actions'
+import { SET_CLIENTS, SET_GARAGE_CONTRACTS }  from '../actions/clients.actions'
 
-const defaultState =  { clients: [] }
+const defaultState =  { clients:         []
+                      , garageContracts: []
+                      }
 
 
 export default function clients (state = defaultState, action) {
   switch (action.type) {
 
     case SET_CLIENTS:
-    return  { ...state
-            , clients: action.value
-            }
+      return  { ...state
+              , clients: action.value
+              }
+
+    case SET_GARAGE_CONTRACTS:
+      return  { ...state
+              , garageContracts: action.value
+              }
 
     default:
       return state
