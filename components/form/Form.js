9ae1657a6bf9759b2935455f87f2e427864de162
Ing. Tomas Hrstka <hrstkatomas@seznam.cz>
2017-06-30 17:15:27 +0200

    Form now prevents double submiting itself

--- components/form/Form.js
+++ components/form/Form.js
@@ -5,25 +5,51 @@ import RoundButton  from '../buttons/RoundButton'
 import styles       from './Form.scss'
 
 
-export default function Form ({ children, onSubmit, onHighlight, onBack, submitable, mobile })  {
-  const sendReservation = () => { submitable && onSubmit() }
-  const highlightInputs = () => { onHighlight && onHighlight() }
+export default class Form extends Component {
+  static propTypes = {
+    onSubmit:    PropTypes.func.isRequired,
+    onHighlight: PropTypes.func,
+    onBack:      PropTypes.func,
+    submitable:  PropTypes.bool,
+    mobile:      PropTypes.bool
+  }
 
-  const back = typeof onBack === "function"
+  constructor(props) {
+     super(props);
+     this.state = { submited: false }
+  }
 
-  var submitButton =  <div className={ mobile ? styles.mobileSubmitButton : styles.submitButton }>
-                        {back && <div className={styles.floatLeft}>
-                          <RoundButton content={<span className="fa fa-chevron-left" aria-hidden="true"></span>} onClick={onBack}/>
-                        </div>}
-                        <div className={back && styles.floatRight}> 
-                          <RoundButton content={<span className='fa fa-check' aria-hidden="true"></span>} onClick={sendReservation} type='confirm' state={!submitable && 'disabled'} onDisabledClick={highlightInputs}/>
+  componentWillReceiveProps(nextProps){
+
+    this.setState({ submited: false })
+  }
+
+  render(){
+    const { children, onSubmit, onHighlight, onBack, submitable, mobile } = this.props
+    const sendReservation = () => {
+      if (submitable){
+        this.setState({ submited: true })
+        onSubmit()
+      }
+    }
+    const highlightInputs = () => { onHighlight && onHighlight() }
+    const back = typeof onBack === "function"
+
+    var submitButton =  <div className={ mobile ? styles.mobileSubmitButton : styles.submitButton }>
+                          {back && <div className={styles.floatLeft}>
+                            <RoundButton content={<span className="fa fa-chevron-left" aria-hidden="true"></span>} onClick={onBack}/>
+                          </div>}
+                          <div className={back && styles.floatRight}>
+                            {this.state.submited ? <RoundButton content={<span className={`fa fa-spinner ${styles.rotating}`} aria-hidden="true"></span>} onClick={()=>{}} type='confirm' state={'loading'} onDisabledClick={highlightInputs}/>
+                                                 : <RoundButton content={<span className='fa fa-check' aria-hidden="true"></span>} onClick={sendReservation} type='confirm' state={!submitable && 'disabled'} onDisabledClick={highlightInputs}/>}
+                          </div>
                         </div>
-                      </div>
 
-  return(
-    <form onSubmit={sendReservation} >
-      { children }
-      { submitButton }
-    </form>
-  )
+    return(
+      <form onSubmit={sendReservation} >
+        { children }
+        { submitButton }
+      </form>
+    )
+  }
 }
