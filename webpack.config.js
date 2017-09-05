6aa2eb6e2c49c1d169a0a71ba8ab40475937d916
Ing. Tomas Hrstka <tomas.hrstka@factorio.cz>
2017-09-05 10:32:39 +0200

    Change to production webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': JSON.stringify(process.env.RAILS_ENV || 'develop'), // Heroku will have RAILS_ENV variable for Rails server
+        'NODE_ENV': JSON.stringify(process.env.RAILS_ENV || 'production'), // Heroku will have RAILS_ENV variable for Rails server
         'API_ENTRYPOINT': JSON.stringify(process.env.API_ENTRYPOINT || 'http://localhost:3000')
       }
     })
