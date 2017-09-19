6bd431a1fea325017f68ccb824f15d20fa59bd27
Ing. Tomas Hrstka <tomas.hrstka@factorio.cz>
2017-09-19 16:09:05 +0200

    Changes to occupancy looks

--- components/occupancyOverview/OccupancyOverview.js
+++ components/occupancyOverview/OccupancyOverview.js
@@ -54,7 +54,7 @@ export class OccupancyOverview extends Component{
   componentDidUpdate(prevProps){
     if (this.props.places.length > 0){
       const intervalLength = moment(this.props.from).add(1, this.props.duration).diff( moment(this.props.from))
-      const rowWidth = this.tbody.childNodes[0].getBoundingClientRect().width - this.tbody.childNodes[0].childNodes[0].getBoundingClientRect().width
+      const rowWidth = this.tbody.childNodes[0].getBoundingClientRect().width - this.tbody.childNodes[0].childNodes[0].getBoundingClientRect().width - (this.props.duration === "day" ? DAY : this.props.duration == 'week' ? WEEK_DAYS : MONTH_DAYS)
 
       for (var i = 0; i < this.props.places.length; i++) {
         var validReservations = this.props.places[i].reservations.filter((reservation) => {
@@ -73,8 +73,8 @@ export class OccupancyOverview extends Component{
           const reservation = validReservations[j]
           var reservationStart = (moment(reservation.begins_at).diff(this.props.from)/intervalLength)*rowWidth
           var reservationEnd = (moment(reservation.ends_at).diff(this.props.from)/intervalLength)*rowWidth
-          reservationStart = reservationStart < 0 ? 0 : (reservationStart - 2)
-          reservationEnd = reservationEnd > rowWidth ? rowWidth : (reservationEnd - 4)
+          reservationStart = reservationStart < 0 ? 0 : (reservationStart - 1.5)
+          reservationEnd = reservationEnd > rowWidth ? rowWidth+30 : (reservationEnd - 2)
 
           moment().isBefore(moment(reservation.begins_at)) ? styles.future : ''
           moment().isBetween(moment(reservation.begins_at), moment(reservation.ends_at)) ? styles.ongoing: ''
