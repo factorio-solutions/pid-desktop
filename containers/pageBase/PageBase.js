dbf2be4edb41662e9fe101810b4f273fd8131e81
Ing. Tomas Hrstka <tomas.hrstka@factorio.cz>
2017-11-21 14:51:16 +0100

    begining of PID admin, users page, users list

--- containers/pageBase/PageBase.js
+++ containers/pageBase/PageBase.js
@@ -6,7 +6,7 @@ import MasterPage from '../../components/masterPage/MasterPage'
 import Modal      from '../../components/modal/Modal'
 import RoundButton from '../../components/buttons/RoundButton'
 
-import {t}      from '../../modules/localization/localization'
+import { t }      from '../../modules/localization/localization'
 import * as nav from '../../helpers/navigation'
 
 import styles from './PageBase.scss'
@@ -22,74 +22,69 @@ class PageBase extends Component {
     notifications: PropTypes.object
   }
 
-  componentDidMount () {
+  componentDidMount() {
     window.scrollTo(0, 0)
     this.props.actions.initialPageBase()
   }
 
-  componentWillReceiveProps(nextProps){ // load garage if id changed
-    nextProps.state.garage != this.props.state.garage && this.props.actions.initialPageBase()
+  componentWillReceiveProps(nextProps) { // load garage if id changed
+    nextProps.state.garage !== this.props.state.garage && this.props.actions.initialPageBase()
   }
 
-  render () {
+  render() {
     const { state, actions, notifications } = this.props
 
-    // console.log(state);
+    const vertical = [
+      { label: t([ 'pageBase', 'Dashboard' ]), key: 'dashboard', icon: 'icon-dashboard', onClick: () => { nav.to('/dashboard') } },
+      { label: t([ 'pageBase', 'Reservation' ]), key: 'reservations', icon: 'icon-reservations', onClick: () => { nav.to('/reservations') } },
+      (state.isGarageAdmin || state.isGarageReceptionist || state.isGarageSecurity) &&
+        { label: t([ 'pageBase', 'Occupancy' ]), key: 'occupancy', icon: 'icon-occupancy', onClick: () => { nav.to(`/${state.garage}/occupancy`) } }, // edit preferences in pageBase.action too
+      (state.isGarageAdmin || state.isGarageReceptionist || state.isGarageSecurity) &&
+        { label: t([ 'pageBase', 'Garage' ]), key: 'garage', icon: 'icon-garage', onClick: () => { nav.to(`/${state.garage}/garage`) } }, // edit preferences in pageBase.action too
+      (state.isGarageAdmin && state.pid_tarif >= 2) &&
+        { label: t([ 'pageBase', 'analytics' ]), key: 'analytics', icon: 'icon-invoices', onClick: () => { actions.analyticsClick() } }, // edit preferences in pageBase.action too
+      // { label: t([ 'pageBase', 'Issues' ]), key: 'issues', icon: 'icon-issues', onClick: () => { nav.to(`/${state.garage}/issues`) } },
+      { label: t([ 'pageBase', 'Admin' ]), key: 'admin', icon: 'icon-admin', onClick: () => { actions.adminClick() } }
+    ].filter(field => field) // will filter false states out
 
-    const vertical =  [{label: t(['pageBase','Dashboard']),    key: "dashboard",    icon: 'icon-dashboard',    onClick: ()=>{nav.to(`/dashboard`)} }
-                    ,  {label: t(['pageBase', 'Reservation']), key: "reservations", icon: 'icon-reservations', onClick: ()=>{nav.to('/reservations')} }
-                    ,  (state.isGarageAdmin || state.isGarageReceptionist || state.isGarageSecurity) && {label: t(['pageBase', 'Occupancy']), key: "occupancy", icon: 'icon-occupancy', onClick: ()=>{nav.to(`/${state.garage}/occupancy`)} } // edit preferences in pageBase.action too
-                    ,  (state.isGarageAdmin || state.isGarageReceptionist || state.isGarageSecurity) && {label: t(['pageBase', 'Garage']),    key: "garage",    icon: 'icon-garage',    onClick: ()=>{nav.to(`/${state.garage}/garage`)} } // edit preferences in pageBase.action too
-                    ,  (state.isGarageAdmin && state.pid_tarif >= 2) && {label: t(['pageBase', 'analytics']), key: "analytics", icon: 'icon-invoices', onClick: ()=>{actions.analyticsClick()} } // edit preferences in pageBase.action too
-                    // ,  {label: t(['pageBase', 'Issues']),       key: "issues",        icon: 'icon-issues', onClick: ()=>{nav.to(`/${state.garage}/issues`)} }
-                    ,  {label: t(['pageBase', 'Admin']), key: "admin", icon: 'icon-admin', onClick: ()=>{actions.adminClick()} }
-                    ].filter(field => field !== false)
+    const callToAction = [
+      { label: t([ 'pageBase', 'Create reservation' ]), onClick: () => { nav.to('/reservations/newReservation') } },
+      state.isGarageAdmin && { label: t([ 'pageBase', 'Create contract' ]), onClick: () => { nav.to(`/${state.garage}/admin/clients/newContract`) } }
+      // { label: t([ 'pageBase', 'Add Features' ]), onClick: () => { nav.to('/addFeatures') } }
+    ].filter(field => field)
 
-    const callToAction = [ {label: t(['pageBase', 'Create reservation']),                      onClick: ()=>{nav.to('/reservations/newReservation')}}
-                         , state.isGarageAdmin &&  {label: t(['pageBase', 'Create contract']), onClick: ()=>{nav.to(`/${state.garage}/admin/clients/newContract`)}}
-                        //  , {label: t(['pageBase', 'Add Features']),                            onClick: ()=>{nav.to('/addFeatures')}}
-                         ].filter(field => field !== false)
+    const profileDropdown = [
+      <div className={styles.dropdownContent} onClick={() => { nav.to('/profile') }}><i className="icon-profile" aria-hidden="true" />{t([ 'pageBase', 'Profile' ])}</div>,
+      state.current_user && state.current_user.pid_admin &&
+        <div className={styles.dropdownContent} onClick={() => { nav.to('/pid-admin') }}><i className="fa fa-wrench" aria-hidden="true" />{t([ 'pageBase', 'pidAdmin' ])}</div>,
+      <div className={styles.dropdownContent} onClick={() => { actions.logout() }}><i className="fa fa-sign-out" aria-hidden="true" />{t([ 'pageBase', 'Logout' ])}</div>
+    ].filter(field => field)
 
-    const profileDropdown = [ <div className={styles.dropdownContent} onClick={()=>{nav.to('/profile')}}><i className="icon-profile" aria-hidden="true"></i>{t(['pageBase', 'Profile'])}</div>
-                            , <div className={styles.dropdownContent} onClick={()=>{actions.logout()}}><i className="fa fa-sign-out" aria-hidden="true"></i>{t(['pageBase', 'Logout'])}</div>
-                            ]
+    const notificationsModalClick = () => actions.setShowModal(false)
 
-    const hint = (state.current_user && state.current_user.hint && state.hint) ? {hint: state.hint, href: state.hintVideo} : undefined
+    const errorContent = (<div style={{ textAlign: 'center' }}>
+      {t([ 'pageBase', 'error' ])}: <br />
+      { state.error } <br />
+      <RoundButton content={<i className="fa fa-check" aria-hidden="true" />} onClick={actions.setError} type="confirm" />
+    </div>)
 
-    const errorClick = () => {
-      actions.setError(undefined)
-    }
-    const successClick = () => {
-      actions.setSuccess(undefined)
-    }
+    const successContent = (<div style={{ textAlign: 'center' }}>
+      {t([ 'pageBase', 'success' ])}: <br />
+      { state.success } <br />
+      <RoundButton content={<i className="fa fa-check" aria-hidden="true" />} onClick={actions.setSuccess} type="confirm" />
+    </div>)
 
-    const notificationsModalClick = () => {
-      actions.setShowModal(false)
-    }
-
-    const errorContent =  <div style={{"textAlign": "center"}}>
-                            {t(['pageBase', 'error'])}: <br/>
-                            { state.error } <br/>
-                            <RoundButton content={<i className="fa fa-check" aria-hidden="true"></i>} onClick={errorClick} type='confirm'  />
-                          </div>
-
-    const successContent =  <div style={{"textAlign": "center"}}>
-                            {t(['pageBase', 'success'])}: <br/>
-                            { state.success } <br/>
-                            <RoundButton content={<i className="fa fa-check" aria-hidden="true"></i>} onClick={successClick} type='confirm'  />
-                          </div>
-
-    const notificationsModal = <div style={{"textAlign": "center"}}>
-                                {t(['pageBase', 'unredNotifications'], {count: notifications.count})}. <br/>
-                                <RoundButton content={<i className="fa fa-check" aria-hidden="true"></i>} onClick={notificationsModalClick} type='confirm'  />
-                              </div>
+    const notificationsModal = (<div style={{ textAlign: 'center' }}>
+      {t([ 'pageBase', 'unredNotifications' ], { count: notifications.count })}. <br />
+      <RoundButton content={<i className="fa fa-check" aria-hidden="true" />} onClick={notificationsModalClick} type="confirm" />
+    </div>)
 
     return (
       <div>
-        <Modal content={errorContent} show={state.error!=undefined} />
-        <Modal content={successContent} show={state.success!=undefined} />
-        <Modal content={notificationsModal} show={notifications.count>0 && state.notificationsModal } />
-        <Modal content={state.custom_modal} show={state.custom_modal!=undefined} />
+        <Modal content={errorContent} show={state.error !== undefined} />
+        <Modal content={successContent} show={state.success !== undefined} />
+        <Modal content={notificationsModal} show={notifications.count > 0 && state.notificationsModal} />
+        <Modal content={state.custom_modal} show={state.custom_modal !== undefined} />
         <MasterPage
           name={state.current_user && state.current_user.full_name}
           messageCount={notifications.count}
@@ -113,6 +108,6 @@ class PageBase extends Component {
 }
 
 export default connect(
-  state    => ({ state: state.pageBase, notifications: state.notifications }),
-  dispatch => ({ actions: bindActionCreators(Object.assign({}, pageBaseActions, loginActions), dispatch) })
+  state => ({ state: state.pageBase, notifications: state.notifications }),
+  dispatch => ({ actions: bindActionCreators({ ...pageBaseActions, ...loginActions }, dispatch) })
 )(PageBase)
