6eaf74d84bec7178e1432020629eaf5be6d9acba
Ing. Tomas Hrstka <tomas.hrstka@factorio.cz>
2017-11-29 10:18:54 +0100

    changes to active status in admin/users accordng to trello card

--- app/admin/users/users.page.js
+++ app/admin/users/users.page.js
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react'
 import { connect }                     from 'react-redux'
 import { bindActionCreators }          from 'redux'
 import moment                          from 'moment'
@@ -17,12 +17,14 @@ import * as usersActions from '../../_shared/actions/users.actions'
 
 import styles from './users.page.scss'
 
+const INACTIVE_AFTER = 60 // days
+
 
 class UsersPage extends Component {
   static propTypes = {
-    state:        PropTypes.object,
-    pageBase:     PropTypes.object,
-    actions:      PropTypes.object
+    state:    PropTypes.object,
+    pageBase: PropTypes.object,
+    actions:  PropTypes.object
   }
 
   componentDidMount() {
@@ -30,79 +32,88 @@ class UsersPage extends Component {
   }
 
   render() {
-    const {state, pageBase, actions} = this.props
+    const { state, pageBase, actions } = this.props
 
-    const schema = [ { key: 'full_name',    title: t(['users','username']),     comparator: 'string', representer: o => <strong> {o} </strong>, sort: 'asc' }
-                   , { key: 'email',        title: t(['users','email']),        comparator: 'string' }
-                   , { key: 'phone',        title: t(['users','phone']),        comparator: 'number' }
-                  //  , { key: 'memberSince',  title: t(['users','memberSince']),  comparator: 'date',   representer: o => o ? o.format('ddd DD.MM.') : null }
-                   , { key: 'last_active',  title: t(['users','active']),       comparator: 'date',   representer: o => moment(o).add(1, 'month').diff(moment()) > 0 ? <i className={`fa fa-check-circle ${styles.green}`} aria-hidden="true"/> : <i className={`fa fa-question-circle ${styles.yellow}`} aria-hidden="true" /> }
-                   ]
+    const schema = [
+      { key: 'full_name', title: t([ 'users', 'username' ]), comparator: 'string', representer: o => <strong> {o} </strong>, sort: 'asc' },
+      { key: 'email', title: t([ 'users', 'email' ]), comparator: 'string' },
+      { key: 'phone', title: t([ 'users', 'phone' ]), comparator: 'number' },
+      //  , { key: 'memberSince',  title: t(['users','memberSince']),  comparator: 'date',   representer: o => o ? o.format('ddd DD.MM.') : null }
+      { key:         'last_active',
+        title:       t([ 'users', 'active' ]),
+        comparator:  'date',
+        representer: o => o ?
+          moment().diff(moment(o), 'days') < INACTIVE_AFTER ?
+            <i className={`fa fa-check-circle ${styles.green}`} aria-hidden="true" title={t([ 'users', 'active' ])} /> :
+            <i className={`fa fa-question-circle ${styles.yellow}`} aria-hidden="true" title={t([ 'users', 'notActive' ])} /> :
+            <i className={`fa fa-times-circle ${styles.red}`} aria-hidden="true" title={t([ 'users', 'notConfimed' ])} />
+      }
+    ]
 
-    const clientOnClick = (id) => {nav.to(`/${pageBase.garage}/admin/clients/${id}/users`)}
+    const clientOnClick = id => { nav.to(`/${pageBase.garage}/admin/clients/${id}/users`) }
     const createClientLink = (client, index) => {
-      const onClick = () => {nav.to(`/${pageBase.garage}/admin/clients/${client.id}/users`)}
-      return <li key={index} className={styles.clickable} onClick={() => {clientOnClick(client.id)}}> {client.name}
-        {client.admin     && <span>{t(['users', 'admin'])}</span>}
-        {client.secretary && <span>{t(['users', 'secretary'])}</span>}
-        {client.internal  && <span>{t(['users', 'internal'])}</span>}
-        {client.host      && <span>{t(['users', 'host'])}</span>}
-      </li>
+      const onClick = () => { nav.to(`/${pageBase.garage}/admin/clients/${client.id}/users`) }
+      return (<li key={index} className={styles.clickable} onClick={() => { clientOnClick(client.id) }}> {client.name}
+        {client.admin && <span>{t([ 'users', 'admin' ])}</span>}
+        {client.secretary && <span>{t([ 'users', 'secretary' ])}</span>}
+        {client.internal && <span>{t([ 'users', 'internal' ])}</span>}
+        {client.host && <span>{t([ 'users', 'host' ])}</span>}
+      </li>)
     }
 
-    const carOnClick = (id) => {nav.to(`/profile/cars/${id}/users`)}
+    const carOnClick = id => { nav.to(`/profile/cars/${id}/users`) }
     const createCarLink = (car, index) => {
-      const onClick = () => {nav.to(`/profile/cars/${car.id}/users`)}
-      return <li key={index} className={styles.clickable} onClick={() => {carOnClick(car.id)}}> {car.model}
-        {car.admin     && <span>{t(['users', 'admin'])}</span>}
-      </li>
+      const onClick = () => { nav.to(`/profile/cars/${car.id}/users`) }
+      return (<li key={index} className={styles.clickable} onClick={() => { carOnClick(car.id) }}> {car.model}
+        {car.admin && <span>{t([ 'users', 'admin' ])}</span>}
+      </li>)
     }
 
-    const garageOnClick = (id) => { /*nav.to(`/${pageBase.garage}/admin/clients/${id}/users`)*/ }
+    const garageOnClick = id => { /* nav.to(`/${pageBase.garage}/admin/clients/${id}/users`)*/ }
     const createGarageLink = (garage, index) => {
-      return <li key={index} className={styles.clickable} onClick={()=>{garageOnClick(garage.id)}}>{garage.name}
-        {garage.admin         && <span>{t(['users', 'admin'])}</span>}
-        {garage.security      && <span>{t(['users', 'security'])}</span>}
-        {garage.receptionist  && <span>{t(['users', 'receptionist'])}</span>}
-      </li>
+      return (<li key={index} className={styles.clickable} onClick={() => { garageOnClick(garage.id) }}>{garage.name}
+        {garage.admin && <span>{t([ 'users', 'admin' ])}</span>}
+        {garage.security && <span>{t([ 'users', 'security' ])}</span>}
+        {garage.receptionist && <span>{t([ 'users', 'receptionist' ])}</span>}
+      </li>)
     }
 
-    const usersData = (state.filter == 'all' ? state.users : state.pending ).map((user) => {
+    const usersData = (state.filter == 'all' ? state.users : state.pending).map(user => {
       // user.memberSince = user.clients.length == 0 ? null : user.clients.reduce((max, client)=> { return moment(client.created_at).diff(max) < 0 ? moment(client.created_at) : max}, moment(moment()))
-      user.spoiler = state.filter == 'all'? <div className={styles.spoiler}>
+      user.spoiler = state.filter == 'all' ? (<div className={styles.spoiler}>
         <div>
-          {t(['users','memberOfClients'])}
+          {t([ 'users', 'memberOfClients' ])}
           <ul>
             {user.clients.map(createClientLink)}
           </ul>
         </div>
         <div>
-          {t(['users','memberOfCars'])}
+          {t([ 'users', 'memberOfCars' ])}
           <ul>
             {user.cars.map(createCarLink)}
           </ul>
         </div>
         <div>
-          {t(['users','memberOfGarages'])}
+          {t([ 'users', 'memberOfGarages' ])}
           <ul>
             {user.garages.map(createGarageLink)}
           </ul>
         </div>
-      </div> :
-      <div>
-        {t(['users', user.client && 'invitedToClient' || user.garage && 'invitedToGarage' || user.car && 'invitedToCar'], {date: moment(user.created_at).format('DD.MM.YYYY HH:mm')})}
-        {user.client && <b className={styles.pointer} onClick={()=>{clientOnClick(user.client.id)}}>{user.client.name}</b>}
-        {user.client && user.client.admin     && <span className={styles.rights}>{t(['users', 'admin'])}</span>}
-        {user.client && user.client.secretary && <span className={styles.rights}>{t(['users', 'secretary'])}</span>}
-        {user.client && user.client.internal  && <span className={styles.rights}>{t(['users', 'internal'])}</span>}
-        {user.client && user.client.host      && <span className={styles.rights}>{t(['users', 'host'])}</span>}
-        {user.garage && <b className={styles.pointer} onClick={()=>{garageOnClick(user.garage.id)}}>{user.garage.name}</b>}
-        {user.garage && user.garage.admin         && <span className={styles.rights}>{t(['users', 'admin'])}</span>}
-        {user.garage && user.garage.security      && <span className={styles.rights}>{t(['users', 'security'])}</span>}
-        {user.garage && user.garage.receptionist  && <span className={styles.rights}>{t(['users', 'receptionist'])}</span>}
-        {user.car    && <b className={styles.pointer} onClick={()=>{carOnClick(user.car.id)}}>{user.car.model}</b>}
-        {user.car    && user.car.admin && <span className={styles.rights}>{t(['users', 'admin'])}</span>}
-      </div>
+      </div>) :
+      (<div>
+        {t([ 'users', user.client && 'invitedToClient' || user.garage && 'invitedToGarage' || user.car && 'invitedToCar' ], { date: moment(user.created_at).format('DD.MM.YYYY HH:mm') })}
+        {user.client && <b className={styles.pointer} onClick={() => { clientOnClick(user.client.id) }}>{user.client.name}</b>}
+        {user.client && user.client.admin && <span className={styles.rights}>{t([ 'users', 'admin' ])}</span>}
+        {user.client && user.client.secretary && <span className={styles.rights}>{t([ 'users', 'secretary' ])}</span>}
+        {user.client && user.client.internal && <span className={styles.rights}>{t([ 'users', 'internal' ])}</span>}
+        {user.client && user.client.host && <span className={styles.rights}>{t([ 'users', 'host' ])}</span>}
+        {user.garage && <b className={styles.pointer} onClick={() => { garageOnClick(user.garage.id) }}>{user.garage.name}</b>}
+        {user.garage && user.garage.admin && <span className={styles.rights}>{t([ 'users', 'admin' ])}</span>}
+        {user.garage && user.garage.security && <span className={styles.rights}>{t([ 'users', 'security' ])}</span>}
+        {user.garage && user.garage.receptionist && <span className={styles.rights}>{t([ 'users', 'receptionist' ])}</span>}
+        {user.car && <b className={styles.pointer} onClick={() => { carOnClick(user.car.id) }}>{user.car.model}</b>}
+        {user.car && user.car.admin && <span className={styles.rights}>{t([ 'users', 'admin' ])}</span>}
+      </div>)
       return user
     })
 
@@ -110,25 +121,26 @@ class UsersPage extends Component {
       nav.to(`/${pageBase.garage}/admin/users/invite`)
     }
 
-    const filters = [ <TabButton label={t(['users', 'allKnownUsers'])}      onClick={() => {actions.allClicked()}}     state={state.filter=="all" && 'selected'}/>
-                    , <TabButton label={t(['users', 'pendingInvitations'])} onClick={() => {actions.pendingClicked()}} state={state.filter=="pending" && 'selected'}/>
-                    ]
+    const filters = [
+      <TabButton label={t([ 'users', 'allKnownUsers' ])} onClick={() => { actions.allClicked() }} state={state.filter === 'all' && 'selected'} />,
+      <TabButton label={t([ 'users', 'pendingInvitations' ])} onClick={() => { actions.pendingClicked() }} state={state.filter === 'pending' && 'selected'} />
+    ]
 
     return (
       <PageBase>
-        <TabMenu left={filters}/>
+        <TabMenu left={filters} />
         <div className={styles.tableContainer}>
           <Table schema={schema} data={usersData} />
         </div>
         <div className={styles.addButton}>
-          <RoundButton content={<span className='fa fa-plus' aria-hidden="true"></span>} onClick={toInviteUser} type='action' size='big'/>
+          <RoundButton content={<span className="fa fa-plus" aria-hidden="true" />} onClick={toInviteUser} type="action" size="big" />
         </div>
       </PageBase>
-    );
+    )
   }
 }
 
 export default connect(
-  state    => ({ state: state.users, pageBase: state.pageBase }),
+  state => ({ state: state.users, pageBase: state.pageBase }),
   dispatch => ({ actions: bindActionCreators(usersActions, dispatch) })
 )(UsersPage)
