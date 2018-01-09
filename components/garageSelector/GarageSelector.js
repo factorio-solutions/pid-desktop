f312cafa7ce986e7c4c9c66258b1499982659964
Ing. Tomáš Hrstka <hrstkatomas@seznam.cz>
2018-01-09 09:04:56 +0100

    code refactoring

--- components/garageSelector/GarageSelector.js
+++ components/garageSelector/GarageSelector.js
@@ -2,46 +2,44 @@ import React, { Component, PropTypes } from 'react'
 import { connect }                     from 'react-redux'
 import { bindActionCreators }          from 'redux'
 
-
 import Dropdown from '../dropdown/Dropdown'
 
 import styles from './GarageSelector.scss'
 
 import * as pageBaseActions from '../../actions/pageBase.actions'
-import * as nav from '../../helpers/navigation'
+import { t } from '../../modules/localization/localization'
+
 
 export class GarageSelector extends Component {
   static propTypes = {
-    state:    PropTypes.object,
-    actions:  PropTypes.object
+    state:   PropTypes.object,
+    actions: PropTypes.object
   }
 
-  render(){
+  render() {
     const { state, actions } = this.props
 
-    const content = state.garages.map(user_garage => user_garage.garage)
+    const content = state.garages.map(userGarage => userGarage.garage)
     const selectedIndex = content.findIndex(garage => garage.id === state.garage)
 
     if (content === undefined || content.length === 0 || selectedIndex === -1) return null
 
-    const selected = (object) => {
-      actions.setGarage(object.id)
-    }
+    const selected = object => actions.setGarage(object.id)
 
-    const dropdownContent = content.map((object, index) => {return {label: object.name, onClick: selected.bind(this, object) }})
+    const dropdownContent = content.map(object => ({ label: object.name, onClick: () => selected(object) }))
 
     return (
       <div>
         <div className={styles.img} >
-          <img src={content[selectedIndex].img || './public/garage_icon.jpg'}/>
+          <img src={content[selectedIndex].img || './public/garage_icon.jpg'} />
         </div>
-        <Dropdown label={'t(selectGarage)'} content={dropdownContent} selected={selectedIndex} style={'garageSelector'} position='fixed'/>
+        <Dropdown label={t([ 'occupancy', 'selectGarage' ])} content={dropdownContent} selected={selectedIndex} style="garageSelector" position="fixed" />
       </div>
     )
   }
 }
 
 export default connect(
-  state    => ({ state: state.pageBase }),
+  state => ({ state: state.pageBase }),
   dispatch => ({ actions: bindActionCreators(pageBaseActions, dispatch) })
 )(GarageSelector)
