7366d36060c4f141dd07874035c977fd5e13d7d9
Ing. Tomáš Hrstka <hrstkatomas@seznam.cz>
2018-01-15 16:41:12 +0100

    goPublic pricing refactoring

--- app/_shared/actions/admin.goPublic.actions.js
+++ app/_shared/actions/admin.goPublic.actions.js
@@ -1,59 +1,48 @@
-import update      from 'immutability-helper'
-import { request } from '../helpers/request'
-import { t }       from '../modules/localization/localization'
-import * as nav       from '../helpers/navigation'
+import update        from 'immutability-helper'
 
-import { GET_GARAGE, CREATE_PRICING, UPDATE_PRICING }from '../queries/admin.goPublic.queries.js'
+import { request }   from '../helpers/request'
+import actionFactory from '../helpers/actionFactory'
+import * as nav      from '../helpers/navigation'
+import { t }         from '../modules/localization/localization'
+
+import { setCustomModal } from './pageBase.actions'
+import { GET_GARAGE, CREATE_PRICING, UPDATE_PRICING } from '../queries/admin.goPublic.queries.js'
 
 
-export const ADMIN_GO_PUBLIC_SET_GARAGE     = 'ADMIN_GO_PUBLIC_SET_GARAGE'
-export const ADMIN_GO_PUBLIC_SET_PLACES      = 'ADMIN_GO_PUBLIC_SET_PLACES'
+export const ADMIN_GO_PUBLIC_SET_GARAGE = 'ADMIN_GO_PUBLIC_SET_GARAGE'
+export const ADMIN_GO_PUBLIC_SET_PLACES = 'ADMIN_GO_PUBLIC_SET_PLACES'
 export const ADMIN_GO_PUBLIC_SET_CURRENCIES = 'ADMIN_GO_PUBLIC_SET_CURRENCIES'
 
 
-export function setGarage (value) {
-  return { type: ADMIN_GO_PUBLIC_SET_GARAGE
-         , value
-         }
-}
+export const setGarage = actionFactory(ADMIN_GO_PUBLIC_SET_GARAGE)
+export const setPlaces = actionFactory(ADMIN_GO_PUBLIC_SET_PLACES)
+export const setCurrencies = actionFactory(ADMIN_GO_PUBLIC_SET_CURRENCIES)
 
-export function setPlaces (value) {
-  return { type: ADMIN_GO_PUBLIC_SET_PLACES
-         , value
-         }
-}
 
-export function setCurrencies (value) {
-  return { type: ADMIN_GO_PUBLIC_SET_CURRENCIES
-         , value
-         }
-}
-
-export function togglePlace (id) {
+export function togglePlace(id) {
   return (dispatch, getState) => {
     const places = getState().adminGoPublic.places
     const index = places.findIndex(place => place === id)
     if (index >= 0) {
-      dispatch(setPlaces( places.slice(0,index).concat(places.slice(index + 1)) ))
+      dispatch(setPlaces(places.slice(0, index).concat(places.slice(index + 1))))
     } else {
-      dispatch(setPlaces(update(places, {$push: [id]} )))
+      dispatch(setPlaces(update(places, { $push: [ id ] })))
     }
   }
 }
 
-
-export function initGoPublic () {
+export function initGoPublic() {
   return (dispatch, getState) => {
-    const onSuccess = (response) => {
+    const onSuccess = response => {
       dispatch(setGarage(response.data.garage))
       dispatch(setCurrencies(response.data.currencies))
     }
 
-    getState().pageBase.garage && request(onSuccess, GET_GARAGE, {id: getState().pageBase.garage})
+    getState().pageBase.garage && request(onSuccess, GET_GARAGE, { id: getState().pageBase.garage })
   }
 }
 
-export function setSelectedCurrency(currency){ // set currencies to all places
+export function setSelectedCurrency(currency) { // set currencies to all places
   return (dispatch, getState) => {
     const state = getState().adminGoPublic
     state.garage.floors.forEach(floor =>
@@ -64,7 +53,7 @@ export function setSelectedCurrency(currency){ // set currencies to all places
   }
 }
 
-export function setFlatPrice(value, valid){
+export function setFlatPrice(value, valid) {
   return (dispatch, getState) => {
     const state = getState().adminGoPublic
     state.places.forEach(id => {
@@ -77,7 +66,7 @@ export function setFlatPrice(value, valid){
     })
   }
 }
-export function setExponential12hPrice(value, valid){
+export function setExponential12hPrice(value, valid) {
   return (dispatch, getState) => {
     const state = getState().adminGoPublic
     state.places.forEach(id => {
@@ -86,7 +75,7 @@ export function setExponential12hPrice(value, valid){
     })
   }
 }
-export function setExponentialDayPrice(value, valid){
+export function setExponentialDayPrice(value, valid) {
   return (dispatch, getState) => {
     const state = getState().adminGoPublic
     state.places.forEach(id => {
@@ -95,7 +84,7 @@ export function setExponentialDayPrice(value, valid){
     })
   }
 }
-export function setExponentialWeekPrice(value, valid){
+export function setExponentialWeekPrice(value, valid) {
   return (dispatch, getState) => {
     const state = getState().adminGoPublic
     state.places.forEach(id => {
@@ -104,7 +93,7 @@ export function setExponentialWeekPrice(value, valid){
     })
   }
 }
-export function setExponentialMonthPrice(value, valid){
+export function setExponentialMonthPrice(value, valid) {
   return (dispatch, getState) => {
     const state = getState().adminGoPublic
     state.places.forEach(id => {
@@ -113,7 +102,7 @@ export function setExponentialMonthPrice(value, valid){
     })
   }
 }
-export function setWeekendPricing(value, valid){
+export function setWeekendPricing(value, valid) {
   return (dispatch, getState) => {
     const state = getState().adminGoPublic
     state.places.forEach(id => {
@@ -122,74 +111,73 @@ export function setWeekendPricing(value, valid){
   }
 }
 
-export function changeFloorFrom(value, index){
+export function changeFloorFrom(value, index) {
   return (dispatch, getState) => {
-    dispatch(setFloors(dispatch(updateValue(index, 'from',   value))))
+    dispatch(setFloors(dispatch(updateValue(index, 'from', value))))
   }
 }
 
-function updatePlacePricing( id, key, value){ // if value = undefined, then remove key
+function updatePlacePricing(id, key, value) { // if value = undefined, then remove key
   return (dispatch, getState) => {
     const state = getState().adminGoPublic
     const floorIndex = state.garage.floors.findIndex(floor => { return floor.places.find(place => place.id === id) !== undefined })
     const placeIndex = state.garage.floors[floorIndex].places.findIndex(place => place.id === id)
 
-    let arr = getState().adminGoPublic.garage.floors
-    let newObject = updateFloor(arr[floorIndex], placeIndex, key, value)
-    let newFloors = update(arr, {$splice: [[floorIndex, 1, newObject]] })
+    const arr = getState().adminGoPublic.garage.floors
+    const newObject = updateFloor(arr[floorIndex], placeIndex, key, value)
+    const newFloors = update(arr, { $splice: [ [ floorIndex, 1, newObject ] ] })
 
-    return update(state.garage, {floors: {$set: newFloors}})
+    return update(state.garage, { floors: { $set: newFloors } })
   }
 }
 
-function updateFloor(floor, placeIndex, key, value){
+function updateFloor(floor, placeIndex, key, value) {
   const place = floor.places[placeIndex]
   const newPricing = value ? updateKey(place.pricing, key, value) : removeKey(place.pricing, key)
-  const newPlace = update(place, {pricing: {$set: newPricing}})
-  const newPlaces = update(floor.places, {$splice: [[placeIndex, 1, newPlace]] })
+  const newPlace = update(place, { pricing: { $set: newPricing } })
+  const newPlaces = update(floor.places, { $splice: [ [ placeIndex, 1, newPlace ] ] })
 
-  return update(floor, {places: {$set: newPlaces}})
+  return update(floor, { places: { $set: newPlaces } })
 }
 
-function updateKey(object, key, value){
-  return update(object || {}, {[key]: {$set: value}})
+function updateKey(object, key, value) {
+  return update(object || {}, { [key]: { $set: value } })
 }
 
-function removeKey(object, key){
+function removeKey(object, key) {
   return Object.keys(object)
-               .filter((currentKey) => currentKey !== key)
-               .reduce((newObj, key) => ({...newObj, [key]: object[key]}), {})
+               .filter(currentKey => currentKey !== key)
+               .reduce((newObj, key) => ({ ...newObj, [key]: object[key] }), {})
 }
 
 export function submitPricings() {
   return (dispatch, getState) => {
-
-
-    Promise.all(getState().adminGoPublic.garage.floors.reduce((acc, floor)=> { // find only valid pricings
-      return [...acc, ...floor.places.filter(place => {
-
+    Promise.all(getState().adminGoPublic.garage.floors.reduce((acc, floor) => { // find only valid pricings
+      return [ ...acc, ...floor.places.filter(place => {
         if (place.pricing === null) return false
         if (place.pricing.currency_id === undefined) return false
-        if ((place.pricing.flat_price == undefined || place.pricing.flat_price == '') &&
-          (place.pricing.exponential_12h_price == undefined || place.pricing.exponential_12h_price == '' ||
-          place.pricing.exponential_day_price == undefined || place.pricing.exponential_day_price == '' ||
-          place.pricing.exponential_week_price == undefined || place.pricing.exponential_week_price == '' ||
-          place.pricing.exponential_month_price == undefined || place.pricing.exponential_month_price == '')) return false
+        if ((place.pricing.flat_price === undefined || place.pricing.flat_price === '') &&
+          (place.pricing.exponential_12h_price === undefined || place.pricing.exponential_12h_price === '' ||
+          place.pricing.exponential_day_price === undefined || place.pricing.exponential_day_price === '' ||
+          place.pricing.exponential_week_price === undefined || place.pricing.exponential_week_price === '' ||
+          place.pricing.exponential_month_price === undefined || place.pricing.exponential_month_price === '')) return false
 
         return true
-      })]
+      }) ]
     }, []).map(place => { // create or update all the pricings
-      return new Promise((resolve, reject) => {
-        const onSuccess = (response) => { resolve(response) }
+      return new Promise(resolve => {
+        const onSuccess = response => { resolve(response) }
+        dispatch(setCustomModal(t([ 'newPricing', 'saving' ])))
 
-        if (place.pricing.id){
-          request( onSuccess, UPDATE_PRICING, { id: place.pricing.id, pricing: removeKey(place.pricing, 'id') } )
+        if (place.pricing.id) {
+          request(onSuccess, UPDATE_PRICING, { id: place.pricing.id, pricing: removeKey(place.pricing, 'id') })
         } else {
-          request( onSuccess, CREATE_PRICING, { pricing: {...place.pricing, place_id: place.id}} )
+          request(onSuccess, CREATE_PRICING, { pricing: { ...place.pricing, place_id: place.id } })
         }
       })
     })).then(values => { // resolved
-       nav.to(`/${getState().pageBase.garage}/admin/modules`)
+      dispatch(setCustomModal())
+      nav.to(`/${getState().pageBase.garage}/admin/modules`)
     })
   }
 }
--- app/admin/modules/goPublic.page.js
+++ app/admin/modules/goPublic.page.js
@@ -22,29 +22,33 @@ class GoPublicPage extends Component {
     actions:  PropTypes.object
   }
 
-  componentWillReceiveProps(nextProps){ // load garage if id changed
-    nextProps.pageBase.garage != this.props.pageBase.garage && this.props.actions.initGoPublic()
+  componentDidMount() {
+    this.props.pageBase.garage && this.props.actions.initGoPublic()
   }
 
-  componentDidMount(){
-    this.props.pageBase.garage && this.props.actions.initGoPublic()
+  componentWillReceiveProps(nextProps) { // load garage if id changed
+    nextProps.pageBase.garage !== this.props.pageBase.garage && this.props.actions.initGoPublic()
   }
 
   render() {
     const { state, actions, pageBase } = this.props
 
-    const preparePlaces = floor => {
-      return {... floor, places: floor.places.map(place => { return { ...place, available: true, selected: state.places.includes(place.id)}})}
-    }
+    const preparePlaces = floor => ({
+      ...floor,
+      places: floor.places.map(place => ({
+        ...place,
+        available: true,
+        selected:  state.places.includes(place.id)
+      }))
+    })
 
-    const currencies = () => {
-      return state.currencies.map((currency, index) => {
-        return { label: currency.code, onClick: actions.setSelectedCurrency.bind(this, currency) }
-      })
-    }
+    const currencies = () => state.currencies.map(currency => ({
+      label:   currency.code,
+      onClick: actions.setSelectedCurrency.bind(this, currency)
+    }))
 
-    const submitForm   = () => { actions.submitPricings() }
-    const goBack       = () => { nav.to(`/${pageBase.garage}/admin/modules`) }
+    const submitForm = () => actions.submitPricings()
+    const goBack = () => nav.to(`/${pageBase.garage}/admin/modules`)
     const isSubmitable = () => {
       return state.garage && state.garage.floors.reduce((atLeastOnePricing, floor) => { // at least one pricing has to be complete to submit
         return floor.places.reduce((atLeastOnePricing, place) => {
@@ -60,9 +64,9 @@ class GoPublicPage extends Component {
       }, false)
     }
 
-    const place = state.garage ? state.garage.floors.reduce((acc,floor) => {
-      return [...acc, ...floor.places]
-    }, []).find(place => state.places.includes(place.id)) : undefined //.find(place => {place.id === state.place})
+    const place = state.garage ? state.garage.floors.reduce((acc, floor) => {
+      return [ ...acc, ...floor.places ]
+    }, []).find(place => state.places.includes(place.id)) : undefined
 
     const pricing = place && place.pricing
     const selectedCurrency = pricing ? state.currencies.findIndex(c => pricing.currency_id === c.id) : -1
@@ -72,31 +76,77 @@ class GoPublicPage extends Component {
         <div className={styles.flex}>
           <div className={styles.half}>
             <Form onSubmit={submitForm} submitable={isSubmitable()} onBack={goBack}>
-              {state.places.length === 0 && <div className={styles.dimmer}>{t(['newPricing', 'selectPlace'])}</div>}
+              {state.places.length === 0 && <div className={styles.dimmer}>{t([ 'newPricing', 'selectPlace' ])}</div>}
               <div>
-                <Dropdown label={t(['newPricing', 'selectCurrency'])} content={currencies()} style='light' selected={selectedCurrency}/>
+                <Dropdown label={t([ 'newPricing', 'selectCurrency' ])} content={currencies()} style="light" selected={selectedCurrency} />
               </div>
               <div>
-                <h2>{t(['newPricing', 'flatPrice'])}</h2>
-                <PatternInput onChange={actions.setFlatPrice} label={t(['newPricing', 'flatPrice'])} error={t(['newPricing', 'invalidPrice'])} pattern="^[+]?\d+([,.]\d+)?$" placeholder={t(['newPricing', 'maxPlaceholder'])} value={pricing ? pricing.flat_price||'' : ''}/>
+                <h2>{t([ 'newPricing', 'flatPrice' ])}</h2>
+                <PatternInput
+                  onChange={actions.setFlatPrice}
+                  label={t([ 'newPricing', 'flatPrice' ])}
+                  error={t([ 'newPricing', 'invalidPrice' ])}
+                  pattern="^[+]?\d+([,.]\d+)?$"
+                  placeholder={t([ 'newPricing', 'maxPlaceholder' ])}
+                  value={pricing ? pricing.flat_price || '' : ''}
+                />
               </div>
               <div>
-                <h2>{t(['newPricing', 'exponentialPrice'])}</h2>
-                <PatternInput onChange={actions.setExponential12hPrice}   label={t(['newPricing', '12hPrice'])}   error={t(['newPricing', 'invalidPrice'])} pattern="^[+]?\d+([,.]\d+)?$" placeholder={t(['newPricing', 'maxPlaceholder'])}   value={pricing ? pricing.exponential_12h_price||'' : ''}  />
-                <PatternInput onChange={actions.setExponentialDayPrice}   label={t(['newPricing', 'dayPrice'])}   error={t(['newPricing', 'invalidPrice'])} pattern="^[+]?\d+([,.]\d+)?$" placeholder={t(['newPricing', 'maxPlaceholder'])}   value={pricing ? pricing.exponential_day_price||'' : ''}  />
-                <PatternInput onChange={actions.setExponentialWeekPrice}  label={t(['newPricing', 'weekPrice'])}  error={t(['newPricing', 'invalidPrice'])} pattern="^[+]?\d+([,.]\d+)?$" placeholder={t(['newPricing', 'minPlaceholder'])}   value={pricing ? pricing.exponential_week_price||'' : ''} />
-                <PatternInput onChange={actions.setExponentialMonthPrice} label={t(['newPricing', 'monthPrice'])} error={t(['newPricing', 'invalidPrice'])} pattern="^[+]?\d+([,.]\d+)?$" placeholder={t(['newPricing', 'decayPlaceholder'])} value={pricing ? pricing.exponential_month_price||'' : ''}/>
+                <h2>{t([ 'newPricing', 'exponentialPrice' ])}</h2>
+                <PatternInput
+                  onChange={actions.setExponential12hPrice}
+                  label={t([ 'newPricing', '12hPrice' ])}
+                  error={t([ 'newPricing', 'invalidPrice' ])}
+                  pattern="^[+]?\d+([,.]\d+)?$"
+                  placeholder={t([ 'newPricing', 'maxPlaceholder' ])}
+                  value={pricing ? pricing.exponential_12h_price || '' : ''}
+                />
+                <PatternInput
+                  onChange={actions.setExponentialDayPrice}
+                  label={t([ 'newPricing', 'dayPrice' ])}
+                  error={t([ 'newPricing', 'invalidPrice' ])}
+                  pattern="^[+]?\d+([,.]\d+)?$"
+                  placeholder={t([ 'newPricing', 'maxPlaceholder' ])}
+                  value={pricing ? pricing.exponential_day_price || '' : ''}
+                />
+                <PatternInput
+                  onChange={actions.setExponentialWeekPrice}
+                  label={t([ 'newPricing', 'weekPrice' ])}
+                  error={t([ 'newPricing', 'invalidPrice' ])}
+                  pattern="^[+]?\d+([,.]\d+)?$"
+                  placeholder={t([ 'newPricing', 'minPlaceholder' ])}
+                  value={pricing ? pricing.exponential_week_price || '' : ''}
+                />
+                <PatternInput
+                  onChange={actions.setExponentialMonthPrice}
+                  label={t([ 'newPricing', 'monthPrice' ])}
+                  error={t([ 'newPricing', 'invalidPrice' ])}
+                  pattern="^[+]?\d+([,.]\d+)?$"
+                  placeholder={t([ 'newPricing', 'decayPlaceholder' ])}
+                  value={pricing ? pricing.exponential_month_price || '' : ''}
+                />
               </div>
 
               <div>
-                <h2>{t(['newPricing', 'weekendPrice'])}</h2>
-                <PatternInput onChange={actions.setWeekendPricing} label={t(['newPricing', 'weekendPrice'])} error={t(['newPricing', 'invalidPrice'])} pattern="^[+]?\d+([,.]\d+)?$" placeholder={t(['newPricing', 'maxPlaceholder'])} value={pricing ? pricing.weekend_price||'' : ''}/>
+                <h2>{t([ 'newPricing', 'weekendPrice' ])}</h2>
+                <PatternInput
+                  onChange={actions.setWeekendPricing}
+                  label={t([ 'newPricing', 'weekendPrice' ])}
+                  error={t([ 'newPricing', 'invalidPrice' ])}
+                  pattern="^[+]?\d+([,.]\d+)?$"
+                  placeholder={t([ 'newPricing', 'maxPlaceholder' ])}
+                  value={pricing ? pricing.weekend_price || '' : ''}
+                />
               </div>
             </Form>
           </div>
 
           <div className={styles.half}>
-            <GarageLayout floors={state.garage ? state.garage.floors.map(preparePlaces) : []} showEmptyFloors={true} onPlaceClick={(place) => { actions.togglePlace(place.id) }}/>
+            <GarageLayout
+              floors={state.garage ? state.garage.floors.map(preparePlaces) : []}
+              showEmptyFloors
+              onPlaceClick={place => { actions.togglePlace(place.id) }}
+            />
           </div>
         </div>
       </PageBase>
@@ -105,6 +155,6 @@ class GoPublicPage extends Component {
 }
 
 export default connect(
-  state    => ({ state: state.adminGoPublic, pageBase: state.pageBase }), //{ state: state.dashboard }
-  dispatch => ({ actions: bindActionCreators(goPublicActions, dispatch) }) //{ actions: bindActionCreators(dashboardActions, dispatch) }
+  state => ({ state: state.adminGoPublic, pageBase: state.pageBase }), // { state: state.dashboard }
+  dispatch => ({ actions: bindActionCreators(goPublicActions, dispatch) }) // { actions: bindActionCreators(dashboardActions, dispatch) }
 )(GoPublicPage)
