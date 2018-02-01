9d9e0a3955a93a36c5cb36cfe78d8466aa65711b
Ing. Tomáš Hrstka <hrstkatomas@seznam.cz>
2018-02-01 15:22:33 +0100

    after updating reservation select reservation row

--- app/_shared/actions/newReservation.actions.js
+++ app/_shared/actions/newReservation.actions.js
@@ -524,7 +524,7 @@ export function submitReservation(id) {
         window.location.replace(response.data.create_reservation.payment_url)
       } else {
         dispatch(pageBaseActions.setCustomModal(undefined))
-        nav.to('/reservations')
+        nav.to(`/reservations/find/${response.data.update_reservation.id}`)
         dispatch(clearForm())
       }
     }
