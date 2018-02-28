2fd00669f525d19b3da780c78f3bc04bac9d0403
Ing. Tomáš Hrstka <hrstkatomas@seznam.cz>
2018-02-28 15:59:05 +0100

    removed font size of pay icon

--- app/reservations/reservations.page.js
+++ app/reservations/reservations.page.js
@@ -228,7 +228,7 @@ class ReservationsPage extends Component {
               {!reservation.approved && reservation.client === null &&
               <LabeledRoundButton
                 label={t([ 'reservations', 'payReservation' ])}
-                content={<i className={`icon-pay ${styles.payIcon}`} title="pay" />}
+                content={<i className="icon-pay" title="pay" />}
                 onClick={() => { actions.payReservation(reservation) }}
                 type="action"
               />
