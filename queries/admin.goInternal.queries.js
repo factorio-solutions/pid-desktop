3619d3e7b8191bf6d4bd2a4199d7bd5102122c10
Ing. Tomáš Hrstka <hrstkatomas@seznam.cz>
2018-03-14 10:12:20 +0100

    added settings for goInternal module

---
+++ queries/admin.goInternal.queries.js
@@ -0,0 +1,38 @@
+// get garage scheme
+export const GET_GARAGE = `query Garage($id: Id!) {
+  garage(id: $id) {
+    id
+    floors {
+      label
+      scheme
+      places {
+        id
+        label
+        go_internal
+        pricing{
+          id
+          currency_id
+          flat_price
+          exponential_12h_price
+          exponential_day_price
+          exponential_week_price
+          exponential_month_price
+          weekend_price
+        }
+      }
+    }
+  }
+  currencies{
+    id
+    code
+    symbol
+  }
+}
+`
+
+export const UPDATE_ALL_PLACES = `mutation UpdatePlaces($places: [PlaceInput]!) {
+  update_places(places: $places) {
+    id
+  }
+}
+`
--- reducers/admin.modules.reducer.js
+++ reducers/admin.modules.reducer.js
@@ -1,5 +1,6 @@
 import {
   ADMIN_MODULES_SET_GO_PUBLIC,
+  ADMIN_MODULES_SET_GO_INTERNAL,
   ADMIN_MODULES_SET_FLEXIPLACE,
   ADMIN_MODULES_SET_MARKETING_PAGE,
   ADMIN_MODULES_SET_MARKETING_SHORT_NAME,
@@ -7,47 +8,54 @@ import {
   ADMIN_MODULES_SET_MR_PARKIT_CONNECTION
  }  from '../actions/admin.modules.actions'
 
-const defaultState =  { goPublic: false
-                      , flexiplace: false
-                      , marketing: false
-                      , short_name: undefined
-                      , reservationForm: false
-                      , mrParkitConnection: false
-                      }
+const defaultState = {
+  goPublic:           false,
+  goInternal:         false,
+  flexiplace:         false,
+  marketing:          false,
+  short_name:         undefined,
+  reservationForm:    false,
+  mrParkitConnection: false
+}
 
 
-export default function adminModules (state = defaultState, action) {
+export default function adminModules(state = defaultState, action) {
   switch (action.type) {
 
     case ADMIN_MODULES_SET_GO_PUBLIC:
-      return  { ...state
-              , goPublic: action.value
-              }
+      return { ...state,
+        goPublic: action.value
+      }
+
+    case ADMIN_MODULES_SET_GO_INTERNAL:
+      return { ...state,
+        goInternal: action.value
+      }
 
     case ADMIN_MODULES_SET_FLEXIPLACE:
-      return  { ...state
-              , flexiplace: action.value
-              }
+      return { ...state,
+        flexiplace: action.value
+      }
 
     case ADMIN_MODULES_SET_MARKETING_PAGE:
-      return  { ...state
-              , marketing: action.value
-              }
+      return { ...state,
+        marketing: action.value
+      }
 
     case ADMIN_MODULES_SET_MARKETING_SHORT_NAME:
-      return  { ...state
-              , short_name: action.value
-              }
+      return { ...state,
+        short_name: action.value
+      }
 
     case ADMIN_MODULES_SET_RESERVATION_FORM:
-      return  { ...state
-              , reservationForm: action.value
-              }
+      return { ...state,
+        reservationForm: action.value
+      }
 
     case ADMIN_MODULES_SET_MR_PARKIT_CONNECTION:
-      return  { ...state
-              , mrParkitConnection: action.value
-              }
+      return { ...state,
+        mrParkitConnection: action.value
+      }
 
     default:
       return state
