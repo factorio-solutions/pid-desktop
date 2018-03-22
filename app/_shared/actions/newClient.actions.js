dde49ed4fce33a9645e51cde05eeb9257e01ae58
Ing. Tomáš Hrstka <hrstkatomas@seznam.cz>
2018-03-22 15:03:44 +0100

    Frontend for setting sms api tokens to clients

--- app/_shared/actions/newClient.actions.js
+++ app/_shared/actions/newClient.actions.js
@@ -1,97 +1,49 @@
-import { request } from '../helpers/request'
-import { get }     from '../helpers/get'
-import * as nav    from '../helpers/navigation'
+import { request }   from '../helpers/request'
+import actionFactory from '../helpers/actionFactory'
+import * as nav      from '../helpers/navigation'
 
 import { CREATE_NEW_CLIENT, EDIT_CLIENT_INIT, EDIT_CLIENT_MUTATION, LOAD_INFO_FROM_IC } from '../queries/newClient.queries'
 
-export const SET_CLIENT_NAME        = "SET_CLIENT_NAME"
-export const SET_CLIENT_IC          = "SET_CLIENT_IC"
-export const SET_CLIENT_DIC         = "SET_CLIENT_DIC"
-export const SET_CLIENT_LINE1       = "SET_CLIENT_LINE1"
-export const SET_CLIENT_LINE2       = "SET_CLIENT_LINE2"
-export const SET_CLIENT_CITY        = "SET_CLIENT_CITY"
-export const SET_CLIENT_POSTAL_CODE = "SET_CLIENT_POSTAL_CODE"
-export const SET_CLIENT_STATE       = "SET_CLIENT_STATE"
-export const SET_CLIENT_COUNTRY     = "SET_CLIENT_COUNTRY"
-export const SET_CLIENT_HIGHLIGHT   = "SET_CLIENT_HIGHLIGHT"
-export const CLEAR_CLIENT_FORM      = "CLEAR_CLIENT_FORM"
+export const SET_CLIENT_NAME = 'SET_CLIENT_NAME'
+export const SET_CLIENT_SMS_API_TOKEN = 'SET_CLIENT_SMS_API_TOKEN'
+export const SET_CLIENT_IC = 'SET_CLIENT_IC'
+export const SET_CLIENT_DIC = 'SET_CLIENT_DIC'
+export const SET_CLIENT_LINE1 = 'SET_CLIENT_LINE1'
+export const SET_CLIENT_LINE2 = 'SET_CLIENT_LINE2'
+export const SET_CLIENT_CITY = 'SET_CLIENT_CITY'
+export const SET_CLIENT_POSTAL_CODE = 'SET_CLIENT_POSTAL_CODE'
+export const SET_CLIENT_STATE = 'SET_CLIENT_STATE'
+export const SET_CLIENT_COUNTRY = 'SET_CLIENT_COUNTRY'
+export const SET_CLIENT_HIGHLIGHT = 'SET_CLIENT_HIGHLIGHT'
+export const CLEAR_CLIENT_FORM = 'CLEAR_CLIENT_FORM'
 
 
-export function setName (name){
-  return  { type: SET_CLIENT_NAME
-          , value: name
-          }
-}
+export const setName = actionFactory(SET_CLIENT_NAME)
+export const setSmsApiToken = actionFactory(SET_CLIENT_SMS_API_TOKEN)
+export const setIC = actionFactory(SET_CLIENT_IC)
+export const setDIC = actionFactory(SET_CLIENT_DIC)
+export const setLine1 = actionFactory(SET_CLIENT_LINE1)
+export const setLine2 = actionFactory(SET_CLIENT_LINE2)
+export const setCity = actionFactory(SET_CLIENT_CITY)
+export const setPostalCode = actionFactory(SET_CLIENT_POSTAL_CODE)
+export const setState = actionFactory(SET_CLIENT_STATE)
+export const setCountry = actionFactory(SET_CLIENT_COUNTRY)
+export const setHighlight = actionFactory(SET_CLIENT_HIGHLIGHT)
+export const clearForm = actionFactory(CLEAR_CLIENT_FORM)
 
-export function setIC (IC){
-  return  { type: SET_CLIENT_IC
-          , value: IC
-          }
-}
 
-export function setDIC (DIC){
-  return  { type: SET_CLIENT_DIC
-          , value: DIC
-          }
-}
-
-export function setLine1 (value) {
-  return { type: SET_CLIENT_LINE1
-         , value
-         }
-}
-
-export function setLine2 (value) {
-  return { type: SET_CLIENT_LINE2
-         , value
-         }
-}
-
-export function setCity (value) {
-  return { type: SET_CLIENT_CITY
-         , value
-         }
-}
-
-export function setPostalCode (value) {
-  return { type: SET_CLIENT_POSTAL_CODE
-         , value
-         }
-}
-
-export function setState (value) {
-  return { type: SET_CLIENT_STATE
-         , value
-         }
-}
-
-export function setCountry (value) {
-  return { type: SET_CLIENT_COUNTRY
-         , value
-         }
-}
-
-export function setHighlight (value){
-  return { type: SET_CLIENT_HIGHLIGHT
-         , value
-         }
-}
-
-export function toggleHighlight (){
+export function toggleHighlight() {
   return (dispatch, getState) => {
     dispatch(setHighlight(!getState().newClient.highlight))
   }
 }
 
-export function clearForm (){
-  return  { type: CLEAR_CLIENT_FORM }
-}
-
 
 export function initClient(id) {
   return (dispatch, getState) => {
-    const onSuccess = (response) => {
+    const onSuccess = response => {
       dispatch(setName(response.data.client_users[0].client.name))
+      dispatch(setSmsApiToken(response.data.client_users[0].client.sms_api_token))
       dispatch(setLine1(response.data.client_users[0].client.address.line_1))
       dispatch(setLine2(response.data.client_users[0].client.address.line_2))
       dispatch(setCity(response.data.client_users[0].client.address.city))
@@ -102,73 +54,73 @@ export function initClient(id) {
       dispatch(setDIC(response.data.client_users[0].client.dic))
     }
 
-    request( onSuccess
-           , EDIT_CLIENT_INIT
-           , { id: parseInt(id)}
-           )
+    const currentUser = getState().pageBase.current_user
+    request(
+      onSuccess,
+      EDIT_CLIENT_INIT.replace(currentUser && currentUser.pid_admin ? '' : 'sms_api_token', ''),
+      { id: parseInt(id, 10) }
+    )
   }
 }
 
-export function loadFromIc(){
+export function loadFromIc() {
   return (dispatch, getState) => {
-    const onSuccess = (response) => {
+    const onSuccess = response => {
       try {
         const res = JSON.parse(response.data.ares).Ares_odpovedi.Odpoved.VBAS
 
         dispatch(setName(res.OF))
         dispatch(setLine1(res.AD.UC))
-        dispatch(setCity(res.AA.N ))
+        dispatch(setCity(res.AA.N))
         dispatch(setPostalCode(res.AA.PSC))
         dispatch(setCountry(res.AA.NS))
         dispatch(setDIC(res.DIC))
       } catch (e) {
-        console.log('not able to parse info from ICO', e);
+        console.log('not able to parse info from ICO', e)
       }
     }
 
-    request( onSuccess
+    request(onSuccess
            , LOAD_INFO_FROM_IC
            , { ic: getState().newClient.ic }
            )
+  }
+}
 
+function generateClient(state) {
+  return {
+    name:          state.name,
+    sms_api_token: state.smsApiToken,
+    ic:            state.ic === '' ? null : state.ic,
+    dic:           state.dic === '' ? null : state.dic,
+    address:       {
+      line_1:      state.line_1,
+      line_2:      state.line_2 === '' ? null : state.line_2,
+      city:        state.city,
+      postal_code: state.postal_code,
+      state:       state.state === '' ? null : state.state,
+      country:     state.country
+    }
   }
 }
 
 export function submitNewClient(id) {
   return (dispatch, getState) => {
-    const onSuccess = (response) => {
+    const onSuccess = () => {
       dispatch(clearForm())
       nav.to(`/${getState().pageBase.garage}/admin/clients`)
     }
 
     if (id) { // then edit client
-      request( onSuccess
-             , EDIT_CLIENT_MUTATION
-             , { id: parseInt(id)
-               , client: generateClient(getState().newClient)
-               }
-             , "RenameClient"
-             )
+      request(onSuccess, EDIT_CLIENT_MUTATION, {
+        id:     parseInt(id, 10),
+        client: generateClient(getState().newClient)
+      })
     } else { // then new client
-      request( onSuccess
-             , CREATE_NEW_CLIENT
-             , { client: generateClient(getState().newClient) }
-             , "garageMutations"
-             )
+      request(onSuccess,
+        CREATE_NEW_CLIENT,
+        { client: generateClient(getState().newClient) }
+      )
     }
   }
-}
-
-function generateClient(state){
-  return { name: state.name
-         , ic:   state.ic === "" ? null : state.ic
-         , dic:  state.dic === "" ? null : state.dic
-         , address: { line_1:      state.line_1
-                    , line_2:      state.line_2 === "" ? null : state.line_2
-                    , city:        state.city
-                    , postal_code: state.postal_code
-                    , state:       state.state === "" ? null : state.state
-                    , country:     state.country
-                    }
-         }
 }
--- app/_shared/reducers/newClient.reducer.js
+++ app/_shared/reducers/newClient.reducer.js
@@ -1,5 +1,6 @@
 import {
   SET_CLIENT_NAME,
+  SET_CLIENT_SMS_API_TOKEN,
   SET_CLIENT_IC,
   SET_CLIENT_DIC,
   SET_CLIENT_LINE1,
@@ -12,74 +13,92 @@ import {
   CLEAR_CLIENT_FORM
 }  from '../actions/newClient.actions'
 
-const defaultState =  { name:        ""
-                      , ic:          ""
-                      , dic:         ""
-                      , line_1:      ""
-                      , line_2:      ""
-                      , city:        ""
-                      , postal_code: ""
-                      , state:       ""
-                      , country:     ""
-                      , highlight:   false
-                      }
+const defaultState = {
+  name:        '',
+  smsApiToken: '',
+  ic:          '',
+  dic:         '',
+  line_1:      '',
+  line_2:      '',
+  city:        '',
+  postal_code: '',
+  state:       '',
+  country:     '',
+  highlight:   false
+}
 
 
-export default function newClient (appState = defaultState, action) {
+export default function newClient(appState = defaultState, action) {
   switch (action.type) {
 
     case SET_CLIENT_NAME:
-    return  { ...appState
-            , name: action.value
-            }
+      return {
+        ...appState,
+        name: action.value
+      }
+
+    case SET_CLIENT_SMS_API_TOKEN:
+      return {
+        ...appState,
+        smsApiToken: action.value
+      }
 
     case SET_CLIENT_IC :
-    return { ...appState
-           , ic: action.value
-           }
+      return {
+        ...appState,
+        ic: action.value
+      }
 
     case SET_CLIENT_DIC :
-    return { ...appState
-           , dic: action.value
-           }
+      return {
+        ...appState,
+        dic: action.value
+      }
 
     case SET_CLIENT_LINE1:
-    return { ...appState
-           , line_1: action.value
-           }
+      return {
+        ...appState,
+        line_1: action.value
+      }
 
     case SET_CLIENT_LINE2:
-    return { ...appState
-           , line_2: action.value
-           }
+      return {
+        ...appState,
+        line_2: action.value
+      }
 
     case SET_CLIENT_CITY:
-    return { ...appState
-           , city: action.value
-           }
+      return {
+        ...appState,
+        city: action.value
+      }
 
     case SET_CLIENT_POSTAL_CODE:
-    return { ...appState
-           , postal_code: action.value
-           }
+      return {
+        ...appState,
+        postal_code: action.value
+      }
 
     case SET_CLIENT_STATE:
-    return { ...appState
-           , state: action.value
-           }
+      return {
+        ...appState,
+        state: action.value
+      }
 
     case SET_CLIENT_COUNTRY:
-    return { ...appState
-           , country: action.value
-           }
+      return {
+        ...appState,
+        country: action.value
+      }
 
     case SET_CLIENT_HIGHLIGHT:
-    return { ...appState
-           , highlight: action.value
-           }
+      return {
+        ...appState,
+        highlight: action.value
+      }
 
     case CLEAR_CLIENT_FORM:
-    return defaultState
+      return defaultState
 
     default:
       return appState
