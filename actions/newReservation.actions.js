597315bb72f4027f766ca7e9d7232e2a31b1bf69
Ing. Tomáš Hrstka <hrstkatomas@seznam.cz>
2018-04-04 15:24:36 +0200

    New Reservation form for sending custom sms

--- actions/newReservation.actions.js
+++ actions/newReservation.actions.js
@@ -60,6 +60,8 @@ export const NEW_RESERVATION_SET_DURATION_DATE = 'NEW_RESERVATION_SET_DURATION_D
 export const NEW_RESERVATION_SET_LOADING = 'NEW_RESERVATION_SET_LOADING'
 export const NEW_RESERVATION_SET_HIGHLIGHT = 'NEW_RESERVATION_SET_HIGHLIGHT'
 export const NEW_RESERVATION_SET_ERROR = 'NEW_RESERVATION_SET_ERROR'
+export const NEW_RESERVATION_SET_SELECTED_TEMPLATE = 'NEW_RESERVATION_SET_SELECTED_TEMPLATE'
+export const NEW_RESERVATION_SET_TEMPLATE_TEXT = 'NEW_RESERVATION_SET_TEMPLATE_TEXT'
 export const NEW_RESERVATION_SET_SEND_SMS = 'NEW_RESERVATION_SET_SEND_SMS'
 export const NEW_RESERVATION_CLEAR_FORM = 'NEW_RESERVATION_CLEAR_FORM'
 
@@ -79,6 +81,8 @@ export const setError = actionFactory(NEW_RESERVATION_SET_ERROR)
 export const clearForm = actionFactory(NEW_RESERVATION_CLEAR_FORM)
 export const setLanguage = actionFactory(NEW_RESERVATION_SET_HOST_LANGUAGE)
 export const setSendSms = actionFactory(NEW_RESERVATION_SET_SEND_SMS)
+export const setSelectedTemplate = (value, template) => ({ type: NEW_RESERVATION_SET_SELECTED_TEMPLATE, value, template })
+export const setTemplateText = actionFactory(NEW_RESERVATION_SET_TEMPLATE_TEXT)
 
 const patternInputActionFactory = type => (value, valid) => ({ type, value: { value, valid } })
 export const setHostName = patternInputActionFactory(NEW_RESERVATION_SET_HOST_NAME)
@@ -558,7 +562,8 @@ export function submitReservation(id) {
                ends_at:                  timeToUTC(state.to),
                recurring_rule:           state.useRecurring ? JSON.stringify(state.recurringRule) : undefined,
                recurring_reservation_id: state.recurring_reservation_id,
-               send_sms:                 state.sendSMS
+               send_sms:                 state.sendSMS,
+               sms_text:                 state.templateText
              },
                id
              }
