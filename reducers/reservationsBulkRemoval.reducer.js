5fae055420a811bacce0464da6d49b845dbe332a
Ing. Tomáš Hrstka <hrstkatomas@seznam.cz>
2018-04-13 17:09:13 +0200

    Refactoring Part 1

--- reducers/reservationsBulkRemoval.reducer.js
+++ reducers/reservationsBulkRemoval.reducer.js
@@ -1,55 +1,69 @@
+import moment from 'moment'
+
 import {
-  SET_GARAGES,
-  SET_RESERVATION_TO_BE_REMOVED,
-  SET_AVAILABLE_USERS,
-  SET_USER_ID,
-  SET_TO,
-  SET_FROM
+  SET_BULK_REMOVAL_GARAGES,
+  SET_BULK_REMOVAL_RESERVATION_TO_BE_REMOVED,
+  SET_BULK_REMOVAL_AVAILABLE_USERS,
+  SET_BULK_REMOVAL_USER_ID,
+  SET_BULK_REMOVAL_TO,
+  SET_BULK_REMOVAL_FROM,
+  BULK_REMOVAL_CLEAR_FORM
 }  from '../actions/reservationsBulkRemoval.actions'
+import { MOMENT_DATETIME_FORMAT, ceilTime } from '../helpers/time'
+
 
 const defaultState = {
   availableUsers: [],
   garages:        [],
-  from:           '',
-  to:             '',
+  from:           ceilTime(moment()).format(MOMENT_DATETIME_FORMAT),
+  to:             ceilTime(moment()).add(30, 'm').format(MOMENT_DATETIME_FORMAT),
   toBeRemoved:    [],
   loading:        false,
   userId:         undefined
 }
 
-// TODO: Set loading
-export default function reservations(state = defaultState, action) {
+
+export default function reservationBulkRemoval(state = defaultState, action) {
   switch (action.type) {
 
-    case SET_GARAGES:
-      return { ...state,
+    case SET_BULK_REMOVAL_GARAGES:
+      return {
+        ...state,
         garages: action.value
       }
 
-    case SET_RESERVATION_TO_BE_REMOVED:
-      return { ...state,
+    case SET_BULK_REMOVAL_RESERVATION_TO_BE_REMOVED:
+      return {
+        ...state,
         toBeRemoved: action.value
       }
 
-    case SET_AVAILABLE_USERS:
-      return { ...state,
+    case SET_BULK_REMOVAL_AVAILABLE_USERS:
+      return {
+        ...state,
         availableUsers: action.value
       }
 
-    case SET_USER_ID:
-      return { ...state,
+    case SET_BULK_REMOVAL_USER_ID:
+      return {
+        ...state,
         userId: action.value
       }
 
-    case SET_FROM:
-      return { ...state,
+    case SET_BULK_REMOVAL_FROM:
+      return {
+        ...state,
         from: action.value
       }
 
-    case SET_TO:
-      return { ...state,
+    case SET_BULK_REMOVAL_TO:
+      return {
+        ...state,
         to: action.value
       }
+
+    case BULK_REMOVAL_CLEAR_FORM:
+      return defaultState
 
     default:
       return state
