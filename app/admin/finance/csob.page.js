ba2384da31e43d05d94a0be3c260abeb97fa8c92
Martin Průcha <martin.prucha@factorio.cz>
2018-04-19 17:20:43 +0200

    Add GP webpay payment gate

--- app/admin/finance/csob.page.js
+++ app/admin/finance/csob.page.js
@@ -33,11 +33,11 @@ class CsobPage extends Component {
   render() {
     const { state, pageBase, actions } = this.props
 
-    const goBack = () => { nav.to(`/${pageBase.garage}/admin/finance`)}
+    const goBack = () => { nav.to(`/${pageBase.garage}/admin/finance`) }
     const submitForm = () => { checkSubmitable() && actions.updateCsobAccount() }
 
     const checkSubmitable = () => {
-      if (state.csob_merchant_id=="" || state.csob_private_key=="") return false
+      if (state.csob_merchant_id === '' || state.csob_private_key === '') return false
 
       return true
     }
@@ -45,11 +45,11 @@ class CsobPage extends Component {
     return (
       <PageBase>
         <Form onSubmit={submitForm} submitable={checkSubmitable()} onBack={goBack} onHighlight={actions.toggleHighlight}>
-          <h2>{t(['finance', 'csobPayment'])}</h2>
-          <Input onEnter={submitForm} onChange={actions.setCsobMerchantId} label={t(['newAccount', 'csobMerchantID'])} error={t(['newAccount', 'invalidMerchantId'])} value={state.csob_merchant_id} name="client[merchantId]" placeholder={t(['newAccount', 'csobMerchantIdplaceholder'])} highlight={state.highlight}/>
-          <label className={state.highlight && styles.red}>{t(['finance', 'SelectPrivateKey'])}: </label>
-          <Input style={styles.hidden} onChange={actions.setCsobPrivateKey} label='file' type="file" name={`newAccountPrivateKey`} accept=".key" />
-          <LabeledRoundButton label={t(['finance', 'uploadKey'])} content={<span className='fa fa-file-code-o' aria-hidden="true"></span>} onClick={() => { document.getElementsByName(`newAccountPrivateKey`)[0].click() }} type={state.csob_private_key===""?'action':'confirm'} />
+          <h2>{t([ 'finance', 'csobPayment' ])}</h2>
+          <Input onEnter={submitForm} onChange={actions.setCsobMerchantId} label={t([ 'newAccount', 'csobMerchantID' ])} error={t(['newAccount', 'invalidMerchantId'])} value={state.csob_merchant_id} name="client[merchantId]" placeholder={t([ 'newAccount', 'csobMerchantIdplaceholder' ])} highlight={state.highlight} />
+          <label className={state.highlight && styles.red}>{t([ 'finance', 'SelectPrivateKey' ])}: </label>
+          <Input style={styles.hidden} onChange={actions.setCsobPrivateKey} label="file" type="file" name={`newAccountPrivateKey`} accept=".key" />
+          <LabeledRoundButton label={t([ 'finance', 'uploadKey' ])} content={<span className="fa fa-file-code-o" aria-hidden="true" />} onClick={() => { document.getElementsByName(`newAccountPrivateKey`)[0].click() }} type={state.csob_private_key === '' ? 'action' : 'confirm'} />
         </Form>
       </PageBase>
     )
