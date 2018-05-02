eddad93b6fb0450c81ee109f96f1b4feb9be0aa1
Ing. Tomáš Hrstka <hrstkatomas@seznam.cz>
2018-05-02 16:59:07 +0200

    added gate access logins to pid admin

--- queries/pid-admin.logs.queries.js
+++ queries/pid-admin.logs.queries.js
@@ -11,3 +11,36 @@ export const GET_ADMIN_LOGS = `query{
   }
 }
 `
+
+export const GET_ADMIN_ACCESS_LOGS = `{
+  gate_access_logs {
+    user_phone
+    access_type
+    created_at
+    user {
+      full_name
+      email
+      phone
+    }
+    gate {
+      label
+      phone
+    }
+    reservation {
+      id
+      place {
+        label
+        floor {
+          label
+          garage {
+            name
+          }
+        }
+      }
+      begins_at
+      ends_at
+    }
+  }
+}
+
+`
