b2d1966f9b1fea86427142f8c60f57820eb98c23
Ing. Tomáš Hrstka <hrstkatomas@seznam.cz>
2018-05-11 10:19:50 +0200

    invite users, when multiple users are invited disable name and phone inputs

--- actions/inviteUser.actions.js
+++ actions/inviteUser.actions.js
@@ -198,8 +198,8 @@ export function createNewManagebles() {
         {
           user: {
             email:     email.toLowerCase(),
-            full_name: state.full_name,
-            phone:     state.phone.replace(/\s/g, ''),
+            full_name: emails.length > 1 ? null : state.full_name,
+            phone:     emails.length > 1 ? null : state.phone.replace(/\s/g, ''),
             message:   state.message,
             language:  state.language
           },
