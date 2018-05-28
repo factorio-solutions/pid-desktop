64736749ff67d46cdcfe891511f59404863f72c8
Martin Průcha <martin.prucha@factorio.cz>
2018-05-28 17:32:58 +0200

    Add garage overview page to PID admin

---
+++ actions/pid-admin.garagesOverview.actions.js
@@ -0,0 +1,20 @@
+import request from '../helpers/requestAdmin'
+import actionFactory from '../helpers/actionFactory'
+
+import { GET_ADMIN_GARAGES } from '../queries/pid-admin.garagesOverview.queries'
+
+export const SET_PID_ADMIN_GARAGESOVERVIEW_SET_GARAGES = 'SET_PID_ADMIN_GARAGESOVERVIEW_SET_GARAGES'
+export const SET_PID_ADMIN_GARAGESOVERVIEW_SET_LOADING = 'SET_PID_ADMIN_GARAGESOVERVIEW_SET_LOADING'
+
+export const setGarages = actionFactory(SET_PID_ADMIN_GARAGESOVERVIEW_SET_GARAGES)
+export const setLoading = actionFactory(SET_PID_ADMIN_GARAGESOVERVIEW_SET_LOADING)
+
+export function initState() {
+  return dispatch => {
+    dispatch(setLoading(true))
+    request(GET_ADMIN_GARAGES).then(data => {
+      dispatch(setGarages(data.garages))
+      dispatch(setLoading(false))
+    })
+  }
+}
---
+++ reducers/pid-admin.garagesOverview.reducer.js
@@ -0,0 +1,28 @@
+import {
+  SET_PID_ADMIN_GARAGESOVERVIEW_SET_GARAGES,
+  SET_PID_ADMIN_GARAGESOVERVIEW_SET_LOADING
+} from '../actions/pid-admin.garagesOverview.actions'
+
+const defaultState = {
+  loading: false,
+  garages: []
+}
+
+export default function garagesOverview(state = defaultState, action) {
+  switch (action.type) {
+    case SET_PID_ADMIN_GARAGESOVERVIEW_SET_GARAGES:
+      return {
+        ...state,
+        garages: action.value
+      }
+
+    case SET_PID_ADMIN_GARAGESOVERVIEW_SET_LOADING:
+      return {
+        ...state,
+        loading: action.value
+      }
+
+    default:
+      return state
+  }
+}
