7ff10bb8e4d46e36d5a58197ed9c0c667948f324
Ing. Tomáš Hrstka <hrstkatomas@seznam.cz>
2018-06-14 09:10:45 +0200

    structure for time credit client module

---
+++ reducers/admin.timeCredit.reducer.js
@@ -0,0 +1,20 @@
+// import {}  from '../actions/admin.timeCredit.actions'
+
+
+const defaultState = {
+}
+
+
+export default function timeCredit(state = defaultState, action) {
+  switch (action.type) {
+
+    // case ANALYTICS_SET_RESERVATIONS:
+    //   return {
+    //     ...state,
+    //     reservations: action.value
+    //   }
+
+    default:
+      return state
+  }
+}
