0e051a7733cc0b8976eed8854d54d9ce57a5e52f
Martin Průcha <martin.prucha@factorio.cz>
2018-06-15 16:46:24 +0200

    add force lower case on email inputs

--- components/input/PatternInput.js
+++ components/input/PatternInput.js
@@ -6,18 +6,19 @@ import styles                          from './Input.scss'
 // this way scss can validate if input has something in it
 export default class PatternInput extends Component {
   static propTypes = {
-    label:       PropTypes.string.isRequired, // is the placeholder
-    pattern:     PropTypes.string, // if type not specified can input regex pattern
-    error:       PropTypes.string, // error message if patern not met
-    placeholder: PropTypes.string,
-    onChange:    PropTypes.func, // use if you want to pass value to parent
-    onEnter:     PropTypes.func, // called when enter pressed
-    value:       PropTypes.string,
-    type:        PropTypes.string,
-    align:       PropTypes.string,
-    inlineMenu:  PropTypes.object,
-    highlight:   PropTypes.bool,
-    readOnly:    PropTypes.bool
+    label:          PropTypes.string.isRequired, // is the placeholder
+    pattern:        PropTypes.string, // if type not specified can input regex pattern
+    error:          PropTypes.string, // error message if patern not met
+    placeholder:    PropTypes.string,
+    onChange:       PropTypes.func, // use if you want to pass value to parent
+    onEnter:        PropTypes.func, // called when enter pressed
+    value:          PropTypes.string,
+    type:           PropTypes.string,
+    align:          PropTypes.string,
+    inlineMenu:     PropTypes.object,
+    highlight:      PropTypes.bool,
+    readOnly:       PropTypes.bool,
+    normalizeInput: PropTypes.func
   }
 
   constructor(props) { // just to handle two way databinding
@@ -29,23 +30,36 @@ export default class PatternInput extends Component {
     nextProps.value !== this.state.message && this.setState({ message: nextProps.value })
   }
 
+  handleChange = event => {
+    const { onChange, normalizeInput } = this.props
+    const { target } = event
+
+    if (typeof normalizeInput === 'function') {
+      let caretPosition = target.selectionStart
+      const originalInputLength = target.value.length
+      target.value = normalizeInput(this.input.value)
+      caretPosition -= originalInputLength - target.value.length
+      target.setSelectionRange(caretPosition, caretPosition)
+    }
+
+    this.setState({ message: target.value })
+
+    if (typeof onChange === 'function') {
+      onChange(target.value, target.checkValidity() && target.value !== '')
+    }
+  }
+
+  preventEnter = event => {
+    const key = (typeof event.which === 'number') ? event.which : event.keyCode
+    if (key === 13) {
+      event.preventDefault()
+      typeof (onEnter) === 'function' && this.props.onEnter()
+    }
+  }
+
   render() {
-    const { label, error, pattern, placeholder, onChange, onEnter, inlineMenu, type, highlight, readOnly, align } = this.props
+    const { label, error, pattern, placeholder, inlineMenu, type, highlight, readOnly, align } = this.props
 
-    const handleChange = event => {
-      this.setState({ message: event.target.value })
-      if (typeof onChange === 'function') {
-        onChange(event.target.value, event.target.checkValidity() && event.target.value !== '')
-      }
-    }
-
-    const preventEnter = event => {
-      const key = (typeof event.which === 'number') ? event.which : event.keyCode
-      if (key === 13) {
-        event.preventDefault()
-        typeof (onEnter) === 'function' && onEnter()
-      }
-    }
 
     const isEmpty = () => this.input ? this.input.value === '' : true
 
@@ -55,9 +69,9 @@ export default class PatternInput extends Component {
           pattern={pattern}
           type={type || 'text'}
           value={this.state.message}
-          onChange={handleChange}
+          onChange={this.handleChange}
           placeholder={placeholder}
-          onKeyPress={preventEnter}
+          onKeyPress={this.preventEnter}
           ref={input => { this.input = input }}
           readOnly={readOnly}
         />
