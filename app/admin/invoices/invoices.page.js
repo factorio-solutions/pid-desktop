9631ec95e44b4a5838f6828c0b1488c0a695f598
Ing. Tomáš Hrstka <hrstkatomas@seznam.cz>
2018-06-25 12:55:46 +0200

    invoice payment method readed

--- app/admin/invoices/invoices.page.js
+++ app/admin/invoices/invoices.page.js
@@ -77,7 +77,12 @@ export const prepareInvoice = (invoice, actions, pageBase) => ({
           type="action"
         />
         {!invoice.payed && !invoice.is_storno_invoice && (invoice.client.is_admin || invoice.client.is_secretary) &&
-          <LabeledRoundButton label={t([ 'invoices', 'payInvoice' ])} content={<i className="fa fa-credit-card" aria-hidden="true" />} onClick={() => {}} type="action" />}
+          <LabeledRoundButton
+            label={t([ 'invoices', 'payInvoice' ])}
+            content={<i className="fa fa-credit-card" aria-hidden="true" />}
+            onClick={() => actions.payInvoice(invoice.id)}
+            type="action"
+          />}
         {!invoice.payed && !invoice.is_storno_invoice && invoice.account.garage.is_admin &&
           <LabeledRoundButton
             label={t([ 'invoices', 'sendReminder' ])}
