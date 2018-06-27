63eebcd9151b24d2feaf0a9f3468f3496ffe0f24
Martin Průcha <martin.prucha@factorio.cz>
2018-06-27 13:55:43 +0200

    Add legal documents page in admin section.

---
+++ actions/legalDocuments.actions.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import actionFactory    from '../helpers/actionFactory'
+
+export const LEGAL_DOCUMENTS_SET_SELECTED = 'LEGAL_DOCUMENTS_SET_SELECTED'
+
+export const setSelected = actionFactory(LEGAL_DOCUMENTS_SET_SELECTED)
