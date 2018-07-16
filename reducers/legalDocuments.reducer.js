72b22b06bbe9c4a8f7e4989ea887bdc638901f5a
Ing. Tomáš Hrstka <hrstkatomas@seznam.cz>
2018-07-16 13:39:50 +0200

    added function to GDPR to insert URL instead of .pdf

--- reducers/legalDocuments.reducer.js
+++ reducers/legalDocuments.reducer.js
@@ -2,17 +2,28 @@ import {
   LEGAL_DOCUMENTS_SET_DOCUMENTS,
   LEGAL_DOCUMENTS_SET_DOCUMENTS_TYPES,
   LEGAL_DOCUMENTS_TOGGLE_HIGHLIGHT,
+
+  LEGAL_DOCUMENTS_SHOW_MODAL,
+  LEGAL_DOCUMENTS_SET_DOCUMENTS_NAME,
+  LEGAL_DOCUMENTS_SET_DOCUMENTS_URL,
+  LEGAL_DOCUMENTS_SUBMIT_DOCUMENT,
+
   LEGAL_DOCUMENTS_CLEAR_FORM
 } from '../actions/legalDocuments.actions'
 
 const defaultState = {
   documents:      [],
   documentsTypes: [ 'privacy', 'terms' ],
-  highlight:      false
+  highlight:      false,
+
+  showModal:    '', // type
+  documentName: '',
+  documentURL:  ''
 }
 
 export default function legalDocuments(state = defaultState, action) {
   switch (action.type) {
+
     case LEGAL_DOCUMENTS_SET_DOCUMENTS_TYPES:
       return {
         ...state,
@@ -30,6 +41,34 @@ export default function legalDocuments(state = defaultState, action) {
         ...state,
         highlight: !state.highlight
       }
+
+
+    case LEGAL_DOCUMENTS_SHOW_MODAL:
+      return {
+        ...state,
+        showModal: action.value
+      }
+
+    case LEGAL_DOCUMENTS_SET_DOCUMENTS_NAME:
+      return {
+        ...state,
+        documentName: action.value
+      }
+
+    case LEGAL_DOCUMENTS_SET_DOCUMENTS_URL:
+      return {
+        ...state,
+        documentURL: action.value
+      }
+
+    case LEGAL_DOCUMENTS_SUBMIT_DOCUMENT:
+      return {
+        ...state,
+        showModal:    false,
+        documentName: '',
+        documentURL:  ''
+      }
+
 
     case LEGAL_DOCUMENTS_CLEAR_FORM:
       return defaultState
