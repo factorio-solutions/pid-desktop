3299bc4147000268f28677900c93a0780aa711a6
Martin Průcha <martin.prucha@factorio.cz>
2018-10-02 13:34:33 +0200

    Change activity log table to paginated table in admin->activity log.

--- app/admin/activityLog/activityLog.page.js
+++ app/admin/activityLog/activityLog.page.js
@@ -4,10 +4,11 @@ import { bindActionCreators }          from 'redux'
 import moment                          from 'moment'
 
 import PageBase from '../../_shared/containers/pageBase/PageBase'
-import Table    from '../../_shared/components/table/Table'
 
 import { t }                         from '../../_shared/modules/localization/localization'
 import * as adminActivityLogsActions from '../../_shared/actions/admin.activityLog.actions'
+import PaginatedTable                from '../../_shared/components/table/PaginatedTable'
+import { GET_LOGS_PAGINATION_QUERY } from '../../_shared/queries/admin.activityLog.queries'
 
 
 class ActivityLogPage extends Component {
@@ -17,29 +18,46 @@ class ActivityLogPage extends Component {
     actions:  PropTypes.object
   }
 
-  componentDidMount() {
-    this.props.pageBase.garage && this.props.actions.initLogs()
-  }
-
-  componentWillReceiveProps(nextProps) { // load garage if id changed
-    nextProps.pageBase.garage !== this.props.pageBase.garage && this.props.actions.initLogs()
+  transformData = data => {
+    return data.logs.map(log => ({
+      full_name:  log.user.full_name,
+      email:      log.user.email,
+      model:      log.model,
+      action:     log.action,
+      created_at: log.created_at
+    }))
   }
 
   render() {
-    const { state } = this.props
+    const { state, actions, pageBase } = this.props
 
     const schema = [
-      { key: 'full_name', title: t([ 'activityLog', 'name' ]), comparator: 'string', sort: 'asc' },
-      { key: 'email', title: t([ 'activityLog', 'email' ]), comparator: 'string' },
-      { key: 'model', title: t([ 'activityLog', 'subject' ]), comparator: 'string' },
-      { key: 'action', title: t([ 'activityLog', 'action' ]), comparator: 'string' },
-      { key: 'created_at', title: t([ 'activityLog', 'createdAt' ]), comparator: 'date', representer: o => <span>{ moment(o).format('ddd DD.MM.')} <br /> {moment(o).format('H:mm')}</span> }
+      { key: 'full_name', title: t([ 'activityLog', 'name' ]), comparator: 'string', includes: 'user', orderBy: 'users.full_name', sort: 'asc' },
+      { key: 'email', title: t([ 'activityLog', 'email' ]), comparator: 'string', includes: 'user', orderBy: 'users.email' },
+      { key: 'model', title: t([ 'activityLog', 'subject' ]), comparator: 'string', orderBy: 'model' },
+      { key: 'action', title: t([ 'activityLog', 'action' ]), comparator: 'string', orderBy: 'action' },
+      { key:         'created_at',
+        title:       t([ 'activityLog', 'createdAt' ]),
+        comparator:  'date',
+        representer: o => <span>{ moment(o).format('ddd DD.MM.')} <br /> {moment(o).format('H:mm')}</span>,
+        orderBy:     'created_at'
+      }
     ]
 
     return (
       <PageBase>
-        <Table schema={schema} data={state.logs} />
+        <PaginatedTable
+          query={GET_LOGS_PAGINATION_QUERY}
+          parseMetadata={data => data.logs_metadata}
+          transformData={this.transformData}
+          schema={schema}
+          storeState={actions.setTableState}
+          state={state.tableState}
+          count={40}
+          variables={{ garage_id: pageBase.garage }}
+        />
       </PageBase>
+        // <Table schema={schema} data={state.logs} />
     )
   }
 }
