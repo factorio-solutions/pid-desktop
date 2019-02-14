d8c41effe5c6d0a01ff44d59ed795bc5322c20d8
Martin Průcha <martin.prucha@factorio.cz>
2019-02-14 16:04:29 +0100

    Add Mpi test.

--- app/pidAdmin/news/news.page.js
+++ app/pidAdmin/news/news.page.js
@@ -14,6 +14,7 @@ import { initNewNews }  from '../../_shared/actions/pid-admin.newNews.actions'
 import { t }            from '../../_shared/modules/localization/localization'
 
 import styles from './news.page.scss'
+import Button from '../../_shared/components/buttons/Button'
 
 
 class PidAdminNewsPage extends Component {
@@ -30,10 +31,27 @@ class PidAdminNewsPage extends Component {
     const { actions, state } = this.props
 
     const schema = [
-      { key: 'id', title: t([ 'pidAdmin', 'news', 'id' ]), comparator: 'number', sort: 'asc' },
-      { key: 'label', title: t([ 'pidAdmin', 'news', 'label' ]), comparator: 'string', representer: o => <span dangerouslySetInnerHTML={{ __html: o }} /> },
+      {
+        key:        'id', title:      t([ 'pidAdmin', 'news', 'id' ]), comparator: 'number', sort:       'asc'
+      },
+      {
+        key:         'label', title:       t([ 'pidAdmin', 'news', 'label' ]), comparator:  'string', representer: o => <span dangerouslySetInnerHTML={{ __html: o }} />
+      },
       { key: 'url', title: t([ 'pidAdmin', 'news', 'url' ]), comparator: 'string' },
-      { key: 'created_at', title: t([ 'pidAdmin', 'news', 'createdAt' ]), comparator: 'date', representer: o => <span>{ moment(o).format('ddd DD.MM.')} <br /> {moment(o).format('H:mm')}</span> }
+      {
+        key:         'created_at',
+        title:       t([ 'pidAdmin', 'news', 'createdAt' ]),
+        comparator:  'date',
+        representer: o => (
+          <span>
+            {moment(o).format('ddd DD.MM.')}
+            {' '}
+            <br />
+            {' '}
+            {moment(o).format('H:mm')}
+          </span>
+        )
+      }
     ]
 
     const addNews = () => actions.initNewNews(null, '', '')
@@ -42,21 +60,24 @@ class PidAdminNewsPage extends Component {
       const editNews = () => actions.initNewNews(news.id, news.label, news.url)
       const deleteNews = () => actions.deleteNews(news.id)
 
-      return { ...news,
-        spoiler: <div className={styles.spoiler}>
-          <LabeledRoundButton
-            label={t([ 'pidAdmin', 'news', 'editNews' ])}
-            content={<span className="fa fa-pencil" aria-hidden="true" />}
-            onClick={editNews}
-            type="action"
-          />
-          <LabeledRoundButton
-            label={t([ 'pidAdmin', 'news', 'deleteNews' ])}
-            content={<span className="fa fa-times" aria-hidden="true" />}
-            onClick={deleteNews}
-            type="remove"
-          />
-        </div>
+      return {
+        ...news,
+        spoiler: (
+          <div className={styles.spoiler}>
+            <LabeledRoundButton
+              label={t([ 'pidAdmin', 'news', 'editNews' ])}
+              content={<span className="fa fa-pencil" aria-hidden="true" />}
+              onClick={editNews}
+              type="action"
+            />
+            <LabeledRoundButton
+              label={t([ 'pidAdmin', 'news', 'deleteNews' ])}
+              content={<span className="fa fa-times" aria-hidden="true" />}
+              onClick={deleteNews}
+              type="remove"
+            />
+          </div>
+        )
       }
     }
 
@@ -68,6 +89,10 @@ class PidAdminNewsPage extends Component {
         <div className={styles.addButton}>
           <RoundButton content={<span className="fa fa-plus" aria-hidden="true" />} onClick={addNews} type="action" size="big" />
         </div>
+        <Button
+          content={state.garName}
+          onClick={actions.mpiGarage}
+        />
       </PageBase>
     )
   }
