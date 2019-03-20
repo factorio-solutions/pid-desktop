531dc1105bc3baab258909bfec68f80a249890db
Martin Průcha <martin.prucha@factorio.cz>
2019-03-20 12:48:39 +0100

    Update new version modal.

--- actions/mobile.version.actions.js
+++ actions/mobile.version.actions.js
@@ -10,6 +10,8 @@ import { GET_CURRENT_MOBILE_VERSION } from '../queries/mobile.header.queries'
 import { setCustomModal } from './mobile.header.actions'
 import { t } from '../modules/localization/localization'
 
+const IS_ANDROID = window.cordova && cordova.platformId === 'android'
+
 export const SET_CURRENT_VERSION = 'SET_CURRENT_VERSION'
 export const SET_APP_VERSION = 'SET_APP_VERSION'
 
@@ -34,7 +36,37 @@ function getCurrentMobileVersion(platform) {
 export function showOlderVersionModal() {
   return dispatch => dispatch(setCustomModal(
     <div>
-      <div>{t([ 'mobileApp', 'version', 'oldAppVersion' ])}</div>
+      <div>
+        {t([ 'mobileApp', 'version', 'oldAppVersion' ])}
+        <br />
+        {window.cordova && (
+          <img
+            src={IS_ANDROID ? './public/google+play.png' : './public/apple+store.png'}
+            alt={IS_ANDROID ? 'google play' : 'apple app store'}
+            title={IS_ANDROID ? 'google play' : 'apple app store'}
+            height="44"
+            width="150"
+            style={{ padding: '5px' }}
+            onClick={() => {
+              cordova.plugins.market.open(
+                IS_ANDROID
+                  ? 'com.parkitdirect.client'
+                  : 'id1262918053',
+                {
+                  success: () => {
+                    console.log('Market success.')
+                    dispatch(setCustomModal())
+                  },
+                  error: () => {
+                    console.log('Market error.')
+                    dispatch(setCustomModal())
+                  }
+                }
+              )
+            }}
+          />
+        )}
+      </div>
       <RoundButton
         content={<span className="fa fa-check" aria-hidden="true" />}
         onClick={() => dispatch(setCustomModal())}
