f75d5bd92df9a4e32afb5a7f2e39eb21b92c2d4b
Ing. Tomáš Hrstka <hrstkatomas@seznam.cz>
2019-04-01 12:36:55 +0200

    Marketing can now be set by admin, not by manager

--- app/admin/modules/components/modulesPageBase.js
+++ app/admin/modules/components/modulesPageBase.js
@@ -167,16 +167,16 @@ class ModulesPageBase extends Component {
         {window.location.hash.includes('marketingPage') &&
         <Module
           name={t([ 'modules', 'marketingPage' ])}
-          disabled={userGarage === undefined || !userGarage.manager}
+          disabled={userGarage === undefined || !userGarage.admin}
           actions={[
             <CallToActionButton
               label={t([ 'modules', 'Preview' ])}
-              state={(userGarage === undefined || !userGarage.manager) ? 'disabled' : 'inverted'}
+              state={(userGarage === undefined || !userGarage.admin) ? 'disabled' : 'inverted'}
               onClick={this.toMarketingPreview}
             />,
             <Switch
               on={state.marketing && state.marketing.active_marketing_launched}
-              state={(userGarage === undefined || !userGarage.manager) && 'disabled'}
+              state={(userGarage === undefined || !userGarage.admin) && 'disabled'}
               onClick={actions.toggleMarketing}
             />
           ]}
@@ -226,7 +226,7 @@ class ModulesPageBase extends Component {
           (window.location.hash.includes('flexiplace')) ||
           (window.location.hash.includes('3rdPartyIntegration') && !(userGarage === undefined || userGarage.garage.active_pid_tarif_id < 2)) ||
           (window.location.hash.includes('mrParkitIntegration') && !(userGarage === undefined || userGarage.garage.active_pid_tarif_id < 2)) ||
-          (window.location.hash.includes('marketingPage') && !(userGarage === undefined || !userGarage.manager))) &&
+          (window.location.hash.includes('marketingPage') && !(userGarage === undefined || !userGarage.admin))) &&
           children
         }
       </PageBase>
