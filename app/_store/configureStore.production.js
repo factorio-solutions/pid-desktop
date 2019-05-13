97047fe9f129ed33aa8bfb7369785896a7f30f59
Martin Průcha <martin.prucha@factorio.cz>
2019-05-13 16:40:08 +0200

    Update configure store for production.

--- app/_store/configureStore.production.js
+++ app/_store/configureStore.production.js
@@ -3,15 +3,19 @@ import thunk                                      from 'redux-thunk'
 import reduxReset                                 from 'redux-reset'
 import { hashHistory }                            from 'react-router'
 import { enableBatching }                         from 'redux-batched-actions'
-import { routerMiddleware }                       from 'react-router-redux'
+import { createHashHistory }                      from 'history'
+import { routerMiddleware }                       from 'connected-react-router'
+
 import errorSender  from '../_shared/errors/errorSenderMiddleware/actionErrorHeandlerMiddleware'
 
-import rootReducer from '../_app/app.reducer'
+import createRootReducer from '../_app/app.reducer'
+
+export const history = createHashHistory()
 
 const router = routerMiddleware(hashHistory)
 
 const enhancer = compose(applyMiddleware(errorSender, thunk, router), reduxReset())
 
 export default function configureStore(initialState) {
-  return createStore(enableBatching(rootReducer), initialState, enhancer)
+  return createStore(enableBatching(createRootReducer(history)), initialState, enhancer)
 }
